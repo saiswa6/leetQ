0a626a4c217ad9e34f7840a5b6dadaa991d73ecb
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2023-09-19 13:54:02 +0530

    Update 0001-two-sum-03.java

--- 0001-two-sum/0001-two-sum-03.java
+++ 0001-two-sum/0001-two-sum-03.java
@@ -5,7 +5,13 @@ Intuition:
 - We can reduce the lookup time from O(n) to O(1) by trading space for speed. A hash table is well suited for this purpose because it supports fast lookup in near constant time. I say "near" because if a collision occurred, a lookup could degenerate to O(n) time. However, lookup in a hash table should be amortized O(1) time as long as the hash function was chosen carefully.
 
 Algorithm:
-A simple implementation uses two iterations. In the first iteration, we add each element's value as a key and its index as a value to the hash table. Then, in the second iteration, we check if each element's complement (target−nums[i]target - nums[i]target−nums[i]) exists in the hash table. If it does exist, we return current element's index and its complement's index. Beware that the complement must not be nums[i]nums[i]nums[i] itself!
+A simple implementation uses two iterations. In the first iteration, we add each element's value as a key and its index as a value to the hash table. Then, in the second iteration, we check if each element's complement (target−nums[i]) exists in the hash table. If it does exist, we return current element's index and its complement's index. Beware that the complement must not be nums[i] itself!
+
+Complexity Analysis
+Time complexity: O(n).
+We traverse the list containing n elements exactly twice. Since the hash table reduces the lookup time to O(1), the overall time complexity is O(n).
+Space complexity: O(n).
+The extra space required depends on the number of items stored in the hash table, which stores exactly n elements.
 */
 
 class Solution {
