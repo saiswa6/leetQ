f219b4ab70e29f3d010396a7bf63b62953ca74de
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2023-09-22 09:07:25 +0530

    Update and rename 0009-palindrome-number.java to 0009-palindrome-number-02.java

--- 0009-palindrome-number/0009-palindrome-number-02.java
+++ 0009-palindrome-number/0009-palindrome-number-02.java
@@ -1,3 +1,9 @@
+/*
+Approach 2 (My solution) :
+Second idea would be reverting the number itself, and then compare the number with original number, if they are the same, then the number is a palindrome. However, if the reversed number is larger than int.MAX, we will hit integer overflow problem.
+*/
+
+
 class Solution {
     public boolean isPalindrome(int x) {
         if(x < 0)
@@ -12,7 +18,6 @@ class Solution {
             int mod = num % 10;
             num = num / 10;
             result = result * 10 + mod ; 
-            
         }
         return result == x;
     }
