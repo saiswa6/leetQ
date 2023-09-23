494f24d02b33881f075de6b728c2a819e4883b0a
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2023-09-23 11:05:45 +0530

    Time: 1 ms (96.79%), Space: 43.9 MB (48.96%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,5 +1,8 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
+        if(nums.length == 0){
+           return 0;
+        }
         int n = nums.length;
         int writer = 0, reader = 0;
         for(; reader< n ; reader++)
@@ -12,26 +15,5 @@ class Solution {
             }
         }
         return writer + 1;
-
-
-        /*
-        int n = nums.length;
-        int writer = 0, reader = 0;
-        for(; reader< n-1 ; reader++)
-        {
-            if(nums[reader] != nums[reader+1])
-            {
-                nums[writer] = nums[reader];
-                writer++;
-            }
-        }
-        if(reader < n && nums[reader-1] != nums[reader])
-        {
-            nums[writer] = nums[reader];
-            writer++;
-        }
-        return writer;
-        
-        */ 
     }
 }
