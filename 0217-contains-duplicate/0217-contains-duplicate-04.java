be33cfad79b4ab77779c0f6f1b31becad1242f1d
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2023-09-23 22:31:29 +0530

    Update and rename 0217-contains-duplicate.java to 0217-contains-duplicate-04.java

--- 0217-contains-duplicate/0217-contains-duplicate-04.java
+++ 0217-contains-duplicate/0217-contains-duplicate-04.java
@@ -1,3 +1,10 @@
+/*
+Approach 4: Hash Map
+Intuition:
+The hash map approach is similar to the hash set approach but also keeps track of the count of occurrences for each element. It uses a hash map to store the elements as keys and their counts as values. If a duplicate element is encountered (count greater than or equal to 1), it returns true. This approach provides more information than just the presence of duplicates and has a time complexity of O(n).
+*/
+
+//MY SOLUTION
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
