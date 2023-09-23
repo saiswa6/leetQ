b00265e4a9ae79a93e9c005be6656ed5ccdcd9ce
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2023-09-23 18:29:17 +0530

    Time: 0 ms (100%), Space: 43.7 MB (39.78%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -8,14 +8,14 @@ class Solution {
         int writer = 0;
         int frequency = 0;
         int n = nums.length;
-        int lastValue = nums[0];
+        int lastValue = nums[0]; //lastValue is needed when encounters {0,0,1,1}.At this point, reader and wrietr will become same and in first if block, frequency will be greater than 3, so elements 1,1 will be missed. so, Tracking last value is neeeded. 
 
         for(int reader=0;reader<n;reader++)
         {
-            if(lastValue == nums[reader])
+            if(lastValue == nums[reader])// compare each time with lastValue only, not with writer because we are moving writer to next pointer
             {
                 frequency++;
-                if(frequency < 3)
+                if(frequency < 3) // update only when frequency is < 3.
                 {
                     nums[writer] = nums[reader];
                     lastValue = nums[writer];
