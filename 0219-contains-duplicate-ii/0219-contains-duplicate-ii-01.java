785a02faaafb089a9bc8f3f6d5caf0f139c2200c
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2023-09-25 16:18:14 +0530

    Update 0219-contains-duplicate-ii-01.java

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii-01.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii-01.java
@@ -25,3 +25,17 @@ public boolean containsNearbyDuplicate(int[] nums, int k) {
     return false;
 }
 // Time Limit Exceeded.
+
+
+//SOLUTION 2
+public boolean containsNearbyDuplicate(int[] nums, int k) {
+	
+	for (int i = 0; i < nums.length; i++) {
+		for (int j = i + 1; j <= i + k && j < nums.length; j++) {
+			if (nums[i] == nums[j]) 
+				return true;
+		}
+	}
+
+	return false;
+}
