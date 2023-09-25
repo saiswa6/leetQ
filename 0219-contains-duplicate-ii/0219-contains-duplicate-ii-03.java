2ee9ddce64ad2e60b1bceb2584648f8ce3bc7064
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2023-09-25 16:04:57 +0530

    Create 0219-contains-duplicate-ii-03.java

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii-03.java
@@ -0,0 +1,37 @@
+/*
+Approach 3 : USing HashMap
+Hasmap put() returns null when key does not exist earlier. If exists, returns the existing key's value.
+
+Complexity Analysis :
+time O(N)
+space O(N), not as efficient as maintaining a K size of a set
+*/
+
+boolean containsNearbyDuplicate(int[] nums, int k) {
+  if (nums == null || nums.length < 2) 
+    return false;
+  Map<Integer, Integer> indices = new HashMap<>();
+  for (int i = 0; i < nums.length; i++) {
+    Integer lastIndex = indices.put(nums[i], i);
+    if (lastIndex != null && (i - lastIndex) <= k)
+      return true;
+  }
+  return false;
+}
+
+//2nd solution
+/*
+for each n
+    if exists in map, check difference
+    if not exists, put n and index in map */
+
+public boolean containsNearbyDuplicate(int[] nums, int k) {
+    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+    for (int i = 0; i < nums.length; i++) {
+        if (map.containsKey(nums[i])) {
+            if (i - map.get(nums[i]) <= k) return true;
+        }
+        map.put(nums[i], i);
+    }
+    return false;
+}
