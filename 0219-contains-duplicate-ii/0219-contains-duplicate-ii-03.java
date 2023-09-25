e65786f6cdcb11000b57f20f9105c0ccdd82affc
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2023-09-25 16:13:52 +0530

    Update 0219-contains-duplicate-ii-03.java

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii-03.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii-03.java
@@ -29,7 +29,8 @@ public boolean containsNearbyDuplicate(int[] nums, int k) {
     Map<Integer, Integer> map = new HashMap<Integer, Integer>();
     for (int i = 0; i < nums.length; i++) {
         if (map.containsKey(nums[i])) {
-            if (i - map.get(nums[i]) <= k) return true;
+            if (Math.abs(i - map.get(nums[i])) <= k) 
+              return true;
         }
         map.put(nums[i], i);
     }
