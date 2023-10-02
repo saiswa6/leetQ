90a314f5f583a8fec3da8e3885d5b7cc74e02194
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2023-10-02 18:03:02 +0530

    Update 0078-subsets-01.java

--- 0078-subsets/0078-subsets-01.java
+++ 0078-subsets/0078-subsets-01.java
@@ -12,7 +12,7 @@ There are generally three strategies to do it:
 
 APPROACH 1: Cascading
 =====================
-Intuition
+Intuition  -- seems DP Approach
 Let's start from empty subset in output list.
 At each step, one takes new integer into consideration and generates new subsets from the existing ones.
 
@@ -40,3 +40,65 @@ class Solution {
     return output;
   }
 }
+
+
+SIMPLER SOLUTION
+================
+Perhaps it could be made more simple by adding each new decision to the previously made choices.
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>());
+        if(nums == null || nums.length == 0){
+            return result;
+        }
+        int s = 0;
+        for(int n:nums){
+            s = result.size();
+            for(int i = 0;i<s;i++){
+                List<Integer> set = new ArrayList<>(result.get(i));
+                set.add(n);
+                result.add(set);
+            }
+        }
+        return result;
+    }
+}
+
+Explanation :
+ArrayList(Collection c):
+
+This constructor is used to build an array list initialized with the elements from the collection c. Suppose, we wish to create an arraylist arr which contains the elements present in the collection c, then, it can be created as:
+
+ArrayList arr = new ArrayList(c);
+
+//So here we are getting result list elements at index i.
+List set = new ArrayList<>(result.get(i));
+
+//Then we are appending the 'n' to above list.
+set.add(n);
+
+n = 1
+s = 1
+result.get(i) = []
+result = [[], [1]]
+
+n = 2
+s = 2
+result.get(i) = []
+result = [[], [1], [2]]
+result.get(i) = [1]
+result = [[], [1], [2], [1, 2]]
+
+n = 3
+s = 4
+result.get(i) = []
+result = [[], [1], [2], [1, 2], [3]]
+result.get(i) = [1]
+result = [[], [1], [2], [1, 2], [3], [1, 3]]
+result.get(i) = [2]
+result = [[], [1], [2], [1, 2], [3], [1, 3], [2, 3]]
+result.get(i) = [1, 2]
+result = [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
+[[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
+
