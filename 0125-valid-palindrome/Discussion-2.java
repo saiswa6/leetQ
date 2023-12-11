48316be6b2d8e5340c34108f7084aaf9bd69dad6
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2023-12-11 17:48:11 +0530

    Update Discussion-2.java

--- 0125-valid-palindrome/Discussion-2.java
+++ 0125-valid-palindrome/Discussion-2.java
@@ -5,3 +5,26 @@ public class Solution {
         return actual.equals(rev);
     }
 }
+
+/////////////////////////////////////////////////////////////////////////////
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        int i = 0;
+        int j = s.length() - 1;
+        while(i <= j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
+
+
+///Note we can save 1 iteration with i < j rather than i <= j because s[i] == s[j] for all i, j where i=j.
+
+
