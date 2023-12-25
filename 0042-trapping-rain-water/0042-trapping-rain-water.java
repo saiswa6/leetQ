0862c756fa7a88f2a0e8dd3aa5d62f066f8db223
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2023-12-25 13:28:16 +0530

    Time: 2 ms (15.93%), Space: 45.8 MB (5.49%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int trap(int[] height) {
+        int length = height.length;
+        int leftarray[] = new int [length];
+        int rightArray[] = new int[length];
+
+        for (int i=0;i<length;i++)
+        {
+            if(i == 0)
+            {
+                leftarray[0] = 0;
+            }
+            else if(i == 1)
+            {
+                leftarray[1] = height[0];
+            }
+            else {
+                leftarray[i] = Math.max(leftarray[i-1], height[i-1]);
+            }
+        }
+
+        for (int i=length - 1;i>=0;i--)
+        {
+            if(i == length - 1)
+            {
+                rightArray[length - 1] = 0;
+            }
+            else if(i == length -2)
+            {
+                rightArray[length - 2] = height[length - 1];
+            }
+            else {
+                rightArray[i] = Math.max(rightArray[i+1], height[i+1]);
+            }
+        }
+        int ans = 0;
+        for(int i = 0; i < length-1;i++)
+        {
+            int smallAns = Math.min(leftarray[i],rightArray[i]) - height[i];
+            ans += (smallAns<0?0:smallAns);
+        }
+        return ans;
+    }
+}
