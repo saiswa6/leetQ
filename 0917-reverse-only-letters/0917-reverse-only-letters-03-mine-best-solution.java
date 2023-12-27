936ea2c98f8df65130e4ea566382e4d4e300de45
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2023-12-27 21:33:52 +0530

    Update and rename 0917-reverse-only-letters-mine-best-solution.java to 0917-reverse-only-letters-03-mine-best-solution.java

--- 0917-reverse-only-letters/0917-reverse-only-letters-03-mine-best-solution.java
+++ 0917-reverse-only-letters/0917-reverse-only-letters-03-mine-best-solution.java
@@ -1,3 +1,6 @@
+t.c o(n)
+s.c o(1)
+
 class Solution {
     public String reverseOnlyLetters(String s) {
         int slength = s.length();
@@ -33,3 +36,19 @@ class Solution {
         }
     }
 }
+
+
+//DISCUSSION
+    public String reverseOnlyLetters(String s) {
+        char temp[] = s.toCharArray();      
+        int low = 0 , high = s.length()-1;
+        while(low < high){
+            if(Character.isAlphabetic(temp[low]) && Character.isAlphabetic(temp[high])){
+                char i = temp[low];temp[low] = temp[high];
+                temp[high] = i;//   Please UPVOTE \U0001f64b‍♂️\U0001f64b\U0001f481\U0001f647\U0001f647\U0001f647‍♂️\U0001f647‍♀️\U0001f647\U0001f647
+                low++; high--;
+            }else if(!Character.isAlphabetic(temp[low]))  low++;
+            else if(!Character.isAlphabetic(temp[high]))  high--;   
+        }
+        return String.valueOf(temp);
+    }
