b58641b5a2a25770666297dc5f0d90bbe1a1bc2b
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-01-01 13:31:46 +0530

    Update and rename 0832-flipping-an-image.java to 0832-flipping-an-image-my-soln.java

--- 0832-flipping-an-image/0832-flipping-an-image-my-soln.java
+++ 0832-flipping-an-image/0832-flipping-an-image-my-soln.java
@@ -1,10 +1,10 @@
 class Solution {
     public int[][] flipAndInvertImage(int[][] image) {
         int n = image[0].length;
-        int mid = n/2;
+        int mid = n/2; // find mid element , n=3=> mid = 1(drawback: for odd, center row is missed for which we have to do negation), n=4 => mid = 2
 
         for (int i=0;i<n;i++) {
-            if(n%2 == 1) {
+            if(n%2 == 1) { // when n is odd, for center, negate, for even it will be covered. 
                 image[i][mid] = image[i][mid] == 0 ? 1 : 0;
             }
             for (int j=0;j<mid;j++) {
