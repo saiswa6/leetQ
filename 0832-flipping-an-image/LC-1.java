9a5a4c70eab711a0d9914883c52867af45116ae0
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-01-01 13:38:40 +0530

    Update LC-1.java

--- 0832-flipping-an-image/LC-1.java
+++ 0832-flipping-an-image/LC-1.java
@@ -10,6 +10,8 @@ Negation using XOR
 
 Complexity Analysis :
 Time Complexity: O(N), where N is the total number of elements in A.
+When you say letters in a big O, you should always clarify what the variables are. The solutions says N is the total number of elements in the array. All elements are processed once. Not sure if this this is why it is thought to be N^2 otherwise.
+If it is meant by rows and columns, then they are not necessarily the same and not N^2 in that case either (except in this problem it is but not in general. here it is also n^2 not N^2 where n is defined in the problem statement).
 Space Complexity: O(1) in additional space complexity.
 */
 
