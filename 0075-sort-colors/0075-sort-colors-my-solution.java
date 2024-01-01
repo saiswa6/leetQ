55007a55b81005b4b21513d7bd62d582efbf7daf
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-01-01 08:24:56 +0530

    Update and rename 0075-sort-colors.java to 0075-sort-colors-my-solution.java

--- 0075-sort-colors/0075-sort-colors-my-solution.java
+++ 0075-sort-colors/0075-sort-colors-my-solution.java
@@ -1,15 +1,15 @@
 class Solution {
     public void sortColors(int[] nums) {
         int length = nums.length;
-        int zeroIndex = 0;
-        int twoIndex = length - 1;
+        int zeroIndex = 0; // track for zero's Index
+        int twoIndex = length - 1;  // track for two's Index
 
         for ( int i=0;i<=twoIndex;i++) {
-            if (nums[i] == 0) {
+            if (nums[i] == 0) {  // If num is 0, then swap i and zeroIndex -> Increase both i and zeroIndex.
                 swap(nums, i, zeroIndex);
                 zeroIndex++;
             }
-            else if (nums[i] == 2) {
+            else if (nums[i] == 2) {  // If num is 0, then swap i and zeroIndex -> Only Increase twoIndex because we are not sure about the element present at twoIndex. will check in next Interation.
                 swap(nums, i, twoIndex);
                 if (nums[i] != 1) {
                     i--;
@@ -17,7 +17,7 @@ class Solution {
                 twoIndex--;
             }
         }
-        /*for ( int j = zeroIndex; j<= twoIndex; j++) {
+        /*for ( int j = zeroIndex; j<= twoIndex; j++) { //This extra step is not needed because swapping will keep 1 in right places.
             nums[j] = 1;
         }*/
     }
