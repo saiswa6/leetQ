a4ae082a84adc6eda03c3a8576f1308bd4c3b094
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-01-01 08:49:24 +0530

    Update LC-Optional.java

--- 0075-sort-colors/LC-Optional.java
+++ 0075-sort-colors/LC-Optional.java
@@ -1,4 +1,4 @@
-// Not appropriate as per solution
+// Not appropriate as per solution----------
 int zero = 0;
     int one = 0;
     int two = 0;
@@ -29,3 +29,24 @@ int zero = 0;
         nums[i] = 2;
     }
 }
+
+(or)
+
+public void sortColors(int[] nums) {
+    // 2-pass
+    int count0 = 0, count1 = 0, count2 = 0;
+    for (int i = 0; i < nums.length; i++) {
+        if (nums[i] == 0) {count0++;}
+        if (nums[i] == 1) {count1++;}
+        if (nums[i] == 2) {count2++;}
+    }
+    for(int i = 0; i < nums.length; i++) {
+        if (i < count0) {nums[i] = 0;}
+        else if (i < count0 + count1) {nums[i] = 1;}
+        else {nums[i] = 2;}
+    }
+}
+
+
+// 2nd solution -----------
+Arrays.sort(nums);
