8415f87129951a510dacd189d2d2b3eb802986a3
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-01-06 13:22:26 +0530

    Create Disc-soln.java

---
+++ 0942-di-string-match/Disc-soln.java
@@ -0,0 +1,8 @@
+    public int[] diStringMatch(String S) {
+        int n = S.length(), left = 0, right = n;
+        int[] res = new int[n + 1];
+        for (int i = 0; i < n; ++i)
+            res[i] = S.charAt(i) == 'I' ? left++ : right--;
+        res[n] = left;
+        return res;
+    }
