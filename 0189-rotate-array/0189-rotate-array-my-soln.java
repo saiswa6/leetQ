6d4a79ec4d7e112d2264ef7e4985cb7521bc0cde
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-01-08 14:42:26 +0530

    Update and rename 0189-rotate-array.java to 0189-rotate-array-my-soln.java

--- 0189-rotate-array/0189-rotate-array-my-soln.java
+++ 0189-rotate-array/0189-rotate-array-my-soln.java
@@ -2,9 +2,9 @@ class Solution {
     public void rotate(int[] nums, int k) {
         int length = nums.length; 
         k = k % length; 
-        reverse(nums, 0, length - 1);
-        reverse(nums, 0, k-1);
-        reverse(nums, k, length-1);
+        reverse(nums, 0, length - 1); // reverse entire array
+        reverse(nums, 0, k-1); // reverse first k elements
+        reverse(nums, k, length-1); // reverse from k to n-1.
     }
 
     public void reverse(int nums[], int start, int end) {
