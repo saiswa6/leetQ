5f6cfb50e27aca7e85315fa279c9e4ec5c5cf7b3
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-01-08 13:03:23 +0530

    Update and rename 1861-rotating-the-box.java to 1861-rotating-the-box-my-solution.java

--- 1861-rotating-the-box/1861-rotating-the-box-my-solution.java
+++ 1861-rotating-the-box/1861-rotating-the-box-my-solution.java
@@ -1,28 +1,30 @@
+// USED STACK TO PROCESS ELEMENTS. A STACK OF COLUMN LENGTH IS NEEDED. 
+
 class Solution {
     public char[][] rotateTheBox(char[][] box) {
-        int m = box.length;
-        int n = box[0].length;
+        int m = box.length;  // row length
+        int n = box[0].length; //column length
         char result [][] = new char[n][m];
 
         for(int i = 0; i < m; i++) {
             Stack<Character> st = process(box[i], n);
             for (int j = 0; j < n ; j++) {
-                result[j][m-i-1] = st.peek();
+                result[j][m-i-1] = st.peek(); // TAKE EACH ELEMENT FROM STACK AND INSERT IN MATRIX.
                 st.pop();
             }
         }
 
         return result;
     }
-
-    public Stack<Character> process(char box[], int n) {
+// IF EMPTY DON'T INSERT.
+    public Stack<Character> process(char box[], int n) { // Process each row from last to first.
         Stack<Character> st = new Stack<>();
         int count = n-1; 
         for(int j = n - 1; j >= 0 ; j--) {
-            if(box[j] == '#') {
+            if(box[j] == '#') {  // IF IS STONE , PUSH IT
                 st.push('#');
                 count--;
-            } else if (box[j] == '*') {
+            } else if (box[j] == '*') { // IF OBSTACLE, BEFORE PUSHING WAIT AND PUSH EMPTY BY COUNT AND THEN PUSH OBSTACLE 
                 while ( count > j) {
                     st.push('.');
                     count--;
@@ -31,7 +33,7 @@ class Solution {
                 count--;
             }
         }
-        while(count >= 0) {
+        while(count >= 0) {  // AT THE END WHATEVER REMAINING, PUSH EMPTY.
             st.push('.');
             count--;
         }
