c03b6b1142f799dcf3d4c44354af9a659d30c7e5
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-01-08 14:21:51 +0530

    Time: 1 ms (54.5%), Space: 57.5 MB (15.83%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -13,10 +13,8 @@ class Solution {
     public void rotate(int nums[], int start, int end) {
         while(start < end) {
             int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-            start++;
-            end--;
+            nums[start++] = nums[end];
+            nums[end--] = temp;
         }
     }
 }
