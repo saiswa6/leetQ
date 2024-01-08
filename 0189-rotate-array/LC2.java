9bca9de4d4aecb4fbb586bc30f3042f3be3da1c9
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-01-08 15:47:23 +0530

    Update LC2.java

--- 0189-rotate-array/LC2.java
+++ 0189-rotate-array/LC2.java
@@ -46,3 +46,43 @@ class Solution {
     }
   }
 }
+
+
+///////  -- Alternative
+    class Solution 
+    {
+    public:
+        void rotate(int nums[], int n, int k) 
+        {
+            if ((n == 0) || (k <= 0))
+            {
+                return;
+            }
+            
+            int cntRotated = 0;
+            int start = 0;
+            int curr = 0;
+            int numToBeRotated = nums[0];
+            int tmp = 0;
+            // Keep rotating the elements until we have rotated n 
+            // different elements.
+            while (cntRotated < n)
+            {
+                do
+                {
+                    tmp = nums[(curr + k)%n];
+                    nums[(curr+k)%n] = numToBeRotated;
+                    numToBeRotated = tmp;
+                    curr = (curr + k)%n;
+                    cntRotated++;
+                } while (curr != start);
+                // Stop rotating the elements when we finish one cycle, 
+                // i.e., we return to start.
+                
+                // Move to next element to start a new cycle.
+                start++;
+                curr = start;
+                numToBeRotated = nums[curr];
+            }
+        }
+    };
