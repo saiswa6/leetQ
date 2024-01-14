2695741d32c8df1d08bdfc336d0a780f366f342e
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-01-14 21:34:05 +0530

    Update 0080-remove-duplicates-from-sorted-array-ii-my-solution.java

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii-my-solution.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii-my-solution.java
@@ -1,14 +1,16 @@
+
+//My Solution
 class Solution {
     public int removeDuplicates(int[] nums) {
 
-        int writeIndex = 0;
+        int writeIndex = 0; // writerIndex maintains the unique values only.
         int length = nums.length;
 
         for(int readIndex = 0; readIndex < length ; ) {
             int count = 0;
-            int firstElement = nums[readIndex];
-            while(readIndex < length && firstElement == nums[readIndex] ) {
-                if(count < 2) {
+            int firstElement = nums[readIndex]; // Take first unnique element into this variable
+            while(readIndex < length && firstElement == nums[readIndex] ) { // Iterate till firstElement is equal
+                if(count < 2) {                     // If the count is less than 2 , update the writeIndex; If greater than 2 , don't update.
                     nums[writeIndex++] = firstElement; 
                     count++;
                 }
@@ -17,9 +19,13 @@ class Solution {
         }
 
         return writeIndex;
+    }
 
-        
-        /*if(nums.length == 0)
+
+//Previous my own written solution
+class Solution {
+    public int removeDuplicates(int[] nums) {
+       if(nums.length == 0)
         {
             return 0;
         }
@@ -27,7 +33,7 @@ class Solution {
         int writer = 0;
         int frequency = 0;
         int n = nums.length;
-        int lastValue = nums[0]; //lastValue is needed when encounters {0,0,1,1}.At this point, reader and wrietr will become same and in first if block, frequency will be greater than 3, so elements 1,1 will be missed. so, Tracking last value is neeeded. 
+        int lastValue = nums[0]; //lastValue is needed when encounters {0,0,1,1}.At this point, reader and writer will become same and in first if block, frequency will be greater than 3, so elements 1,1 will be missed. so, Tracking last value is neeeded. 
 
         for(int reader=0;reader<n;reader++)
         {
@@ -37,7 +43,7 @@ class Solution {
                 if(frequency < 3) // update only when frequency is < 3.
                 {
                     nums[writer] = nums[reader];
-                    lastValue = nums[writer];
+                    lastValue = nums[writer]; // Here, no need to update lastValue.
                     writer++;
                 }
             }
