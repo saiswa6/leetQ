21870102c5026e389ee98877864ba42f0acb32fa
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-01-14 20:57:22 +0530

    Update 0026-remove-duplicates-from-sorted-array-my-solution.java

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array-my-solution.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array-my-solution.java
@@ -1,30 +1,15 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         int length = nums.length;
-        int writerIndex = 0;
-        for(int readIndex = 0; readIndex < length; ) {
-            int firstElement = nums[readIndex];
-             while(readIndex < length && firstElement == nums[readIndex]) {
+        int writerIndex = 0; // maintains unique values by writing
+        for(int readIndex = 0; readIndex < length; ) { // reads all the values of the array.
+            int firstElement = nums[readIndex]; // Take first unique element
+             while(readIndex < length && firstElement == nums[readIndex]) { // Iterate and increment till the firstElement completes.
                  readIndex++;
             }
-            nums[writerIndex++] = firstElement;
+            nums[writerIndex++] = firstElement; // write the unique element
         }
         
         return writerIndex;
-        
-        /*if(nums.length == 0){
-           return 0;
-        }
-        int n = nums.length;
-        int writer = 0, reader = 0;
-        for(; reader< n ; reader++)
-        {
-            if(nums[reader] != nums[writer])
-            {
-                writer++;
-                nums[writer] = nums[reader];      
-            }
-        }
-        return writer + 1;*/
     }
 }
