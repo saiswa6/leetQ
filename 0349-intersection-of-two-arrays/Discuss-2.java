cf611e7fa63b3da3186e251a7c5df8ae246ebe73
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-02-04 10:16:56 +0530

    Create Discuss-2.java

---
+++ 0349-intersection-of-two-arrays/Discuss-2.java
@@ -0,0 +1,112 @@
+/*
+1. Use two hash sets
+Time complexity: O(n)
+*/
+public class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Set<Integer> intersect = new HashSet<>();
+        for (int i = 0; i < nums1.length; i++) {
+            set.add(nums1[i]);
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            if (set.contains(nums2[i])) {
+                intersect.add(nums2[i]);
+            }
+        }
+        int[] result = new int[intersect.size()];
+        int i = 0;
+        for (Integer num : intersect) {
+            result[i++] = num;
+        }
+        return result;
+    }
+}
+
+//2nd approach
+//Sort both arrays, use two pointers
+//Time complexity: O(nlogn)
+
+public class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                set.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        int[] result = new int[set.size()];
+        int k = 0;
+        for (Integer num : set) {
+            result[k++] = num;
+        }
+        return result;
+    }
+}
+
+//3rd case
+//Binary search
+//Time complexity: O(nlogn)
+// A little optimization for the binary search solutions. Compare the length of two arrays, and sort the shorter one.
+/*
+@turingfly
+Very brilliant advice, thank you!
+
+Suppose x = nums1.length, y = nums2.length, and x > y.
+
+The overall time complexity is determined by two major components:
+(1) array sorting, whose time complexity is O(n*log n).
+(2) binary search, whose time complexity is O(log n).
+
+If the shorter array is sorted, then the overall time complexity is O(y*log y + x*log y) = O((x+y)*log y), otherwise O((x+y)*log x), which is greater.
+*/
+
+public class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Arrays.sort(nums2);
+        for (Integer num : nums1) {
+            if (binarySearch(nums2, num)) {
+                set.add(num);
+            }
+        }
+        int i = 0;
+        int[] result = new int[set.size()];
+        for (Integer num : set) {
+            result[i++] = num;
+        }
+        return result;
+    }
+    
+    public boolean binarySearch(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return false;
+    }
+}
+
+
+//Note :
+Converting from set to int [] can be simplified like this using Java 8 Stream:
+ return set.stream().mapToInt(k -> k).toArray();
