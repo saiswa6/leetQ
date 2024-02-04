4694a64036ccd79bd110e734fa563835dcd43996
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-02-04 09:56:53 +0530

    Update and rename 0349-intersection-of-two-arrays.java to 0349-intersection-of-two-arrays-my-soln1.java

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays-my-soln1.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays-my-soln1.java
@@ -1,6 +1,8 @@
+//T.C : O(mlogm + nlogn + (m or n whichever is larger))
+//S.C : O(m) + O(n)
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
+        Arrays.sort(nums1); //Sort the arrays and apply the 2 pointer technique.
         Arrays.sort(nums2);
 
         int length1 = nums1.length;
@@ -11,11 +13,11 @@ class Solution {
         int index = 0;
 
         while(nums1Pointer < length1 && nums2Pointer < length2) {
-            if(nums1[nums1Pointer] < nums2[nums2Pointer] ) {
+            if(nums1[nums1Pointer] < nums2[nums2Pointer] ) { // Increment the less than number
                 nums1Pointer++;
             } else if(nums1[nums1Pointer] > nums2[nums2Pointer] ) {
-                nums2Pointer++;
-            } else {
+                nums2Pointer++; 
+            } else {  // If equal, copy the value to final array and increment both the pointers.
                 ans[index++] = nums1[nums1Pointer];
                 int sameElem1 = nums1[nums1Pointer++];
                 int sameElem2 = nums2[nums2Pointer++];
@@ -28,6 +30,6 @@ class Solution {
             }
         }
 
-        return Arrays.copyOf(ans, index);
+        return Arrays.copyOf(ans, index); // V Imp to copy the no of elements to final array.
     }
 }
