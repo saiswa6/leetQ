7956d5d8cb8fe1a8a3c3ea017f4095946590f66f
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-02-06 10:37:29 +0530

    Update and rename 0925-long-pressed-name.java to My Solution.java

--- 0925-long-pressed-name/My Solution.java
+++ 0925-long-pressed-name/My Solution.java
@@ -1,3 +1,5 @@
+// Two Pointer SOlution
+
 class Solution {
     public boolean isLongPressedName(String name, String typed) {
         char nameArray []= name.toCharArray();
@@ -9,27 +11,27 @@ class Solution {
         int typedPointer = 0;
 
         while(namePointer < nameLength || typedPointer < typedLength) {
-            if(namePointer >= nameLength || typedPointer >= typedLength) {
+            if(namePointer >= nameLength || typedPointer >= typedLength) { // if any one of the arrays left finally, then not aligned properly, SO false.
                 return false;
             }
             int nameChar = nameArray[namePointer];
             int nameCount = 0;
-            while(namePointer < nameLength && nameChar == nameArray[namePointer]) {
+            while(namePointer < nameLength && nameChar == nameArray[namePointer]) { // iterate to count the no of times character repeats to compare below
                 namePointer++;
                 nameCount++;
             }
 
             int typedChar = typedArray[typedPointer];
             int typedCount = 0;
-            while(typedPointer < typedLength && typedChar == typedArray[typedPointer]) {
+            while(typedPointer < typedLength && typedChar == typedArray[typedPointer]) { // iterate to count the no of times character repeats to compare below
                 typedPointer++;
                 typedCount++;
             }
-            if(nameChar != typedChar || nameCount > typedCount) {
+            if(nameChar != typedChar || nameCount > typedCount) { // If char is not same or count of name > count of typed, return false
                 return false;
             }
         }
 
-        return true;
+        return true; // if everything satisfied, return true
     }
 }
