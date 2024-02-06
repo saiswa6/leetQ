f17c7a28cbeeedea9159ec46b2cd769d9a06b64f
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-02-06 06:57:16 +0530

    Update and rename 0160-intersection-of-two-linked-lists.java to MySolution.java

--- 0160-intersection-of-two-linked-lists/MySolution.java
+++ 0160-intersection-of-two-linked-lists/MySolution.java
@@ -9,17 +9,19 @@
  *     }
  * }
  */
+
+// Using HAsh Set
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         Set<ListNode> hs = new HashSet<>();
         ListNode iteratorA = headA;
-        while(iteratorA != null) {
+        while(iteratorA != null) {  // Pass all the elements of Linked List1 to Hash Set
             hs.add(iteratorA);
             iteratorA = iteratorA.next;
         }
 
         ListNode iteratorB = headB;
-        while(iteratorB != null) {
+        while(iteratorB != null) { // Iterate through Linked List2 and see if any element matches with the HashSet. If yes, that's the intersection point.
             if(hs.contains(iteratorB)) {
                 return iteratorB;
             }
