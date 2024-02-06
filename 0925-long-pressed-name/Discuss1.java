9a2b92e56bde28382630d987dff0a6f5035803f9
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-02-06 10:43:37 +0530

    Update Discuss1.java

--- 0925-long-pressed-name/Discuss1.java
+++ 0925-long-pressed-name/Discuss1.java
@@ -37,3 +37,26 @@ class Solution {
         return i == m && j == n;
     }
 }
+
+
+// similar
+
+public boolean isLongPressedName(String nameStr, String typeStr) {
+        char[] name = nameStr.toCharArray(), typed = typeStr.toCharArray();
+        int n = 0, t = 0;
+        while (n < name.length && t < typed.length) {
+            int need = 1;
+            char c = name[n++];
+            while (n < name.length && c == name[n]) {
+                n++;
+                need++;
+            }
+            while (t < typed.length && typed[t] == c) {
+                need--;
+                t++;
+            }
+            if (need > 0)
+                return false;
+        }
+        return n == name.length && t == typed.length;
+    }
