50ed8df51cb5352c70d7a61fcb824f0779929842
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-02-06 06:25:17 +0530

    Update and rename 0028-find-the-index-of-the-first-occurrence-in-a-string.java to My Solution.java

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/My Solution.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/My Solution.java
@@ -1,24 +1,6 @@
+// WRONG SOLUTION
+// This solution is failing in case of missississpi and needle issipi
 class Solution {
-    public int strStr(String haystack, String needle) {
-        int m = needle.length();
-        int n = haystack.length();
-
-        for (int windowStart = 0; windowStart <= n - m; windowStart++) {
-            for (int i = 0; i < m; i++) {
-                if (needle.charAt(i) != haystack.charAt(windowStart + i)) {
-                    break;
-                }
-                if (i == m - 1) {
-                    return windowStart;
-                }
-            }
-        }
-
-        return -1;
-    }
-}
-
-/*class Solution {
     public int strStr(String haystack, String needle) {
         int hayLength = haystack.length();
         int needleLength = needle.length();
@@ -54,4 +36,4 @@ class Solution {
 
         return startPoint;
     }
-}*/
+}
