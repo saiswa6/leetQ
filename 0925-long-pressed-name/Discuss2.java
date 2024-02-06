a5fccd214558d989425b0bd79c5717a60da07832
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-02-06 10:54:45 +0530

    Update Discuss2.java

--- 0925-long-pressed-name/Discuss2.java
+++ 0925-long-pressed-name/Discuss2.java
@@ -5,9 +5,9 @@
     public boolean isLongPressedName(String name, String typed) {
         int i = 0, m = name.length(), n = typed.length();
         for (int j = 0; j < n; ++j) // Iterate through typed array
-            if (i < m && name.charAt(i) == typed.charAt(j))  // First check whether corresponding letters of name and typed are same, if yes, increment name pointer
+            if (i < m && name.charAt(i) == typed.charAt(j))  // First check whether corresponding letters of name and typed are same, if yes, increment name pointer. If no, go down.
                 ++i;
-            else if (j == 0 || typed.charAt(j) != typed.charAt(j - 1)) // Second check whether neighbout letters of typed are same, if no, increment, return false
+            else if (j == 0 || typed.charAt(j) != typed.charAt(j - 1)) // Second check whether neighbour letters of typed are same, if no, return false
                 return false;
         return i == m; // named pointer shall reach end, then only returnn true, otherwise false.
     }
