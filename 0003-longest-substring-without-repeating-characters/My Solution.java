f75d91ae574c34927f597dc5c35f4db016d6f259
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-02-11 15:47:35 +0530

    Update and rename 0003-longest-substring-without-repeating-characters.java to My Solution.java

--- 0003-longest-substring-without-repeating-characters/My Solution.java
+++ 0003-longest-substring-without-repeating-characters/My Solution.java
@@ -9,35 +9,25 @@ class Solution {
         for( ; right < s.length() ; right++) {
             char r = s.charAt(right);
             if(trackMap.containsKey(r)) {
-                trackMap.put(r, trackMap.get(r) + 1);
+                trackMap.put(r, trackMap.get(r) + 1); // Increase frequency of char
                 repeatedChars++;
             } else {
                 trackMap.put(r, 1);
             }
-            //trackMap.put(r, trackMap.getOrDefault(r,0) + 1);
-            // if(trackSet.contains(s.charAt(right))) {
-            //     repeatedChars++;
-            // } 
-            // trackSet.add(s.charAt(right));
 
             while(repeatedChars > 0) {
                 Character leftChar = s.charAt(left);
-                    
                     int count = trackMap.get(leftChar);
                     if(count == 1) {
-                        trackMap.remove(leftChar);
+                        trackMap.remove(leftChar);  // If one element only, then apply, remove completely
                     } else {
                         repeatedChars--;
-                        trackMap.put(leftChar, count - 1);
+                        trackMap.put(leftChar, count - 1); // Reduce the count of leftChar
                     }
-                    
-                    
-
-                
-                left++;
+                left++; // Shrink the window
             }
 
-            answer = Math.max(answer, right-left + 1);
+            answer = Math.max(answer, right-left + 1); // Track for max answer.
         }
         return answer;
     }
