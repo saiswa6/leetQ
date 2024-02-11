9512886153a84b3a91d7ed723c5bc1cf53947243
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-02-11 12:20:08 +0530

    Time: 4 ms (22.49%), Space: 57.1 MB (27.13%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.java
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.java
@@ -9,6 +9,28 @@ class Solution {
                 zeroCount++;
             }
 
+            if(zeroCount > 1) {
+                if(nums[left] == 0) {
+                    zeroCount--;
+                }
+                left++;
+            }
+
+            answer = Math.max(answer, right - left);
+        }
+        return answer;
+
+
+        /* 
+        int answer = 0;
+        int zeroCount = 0;
+        int left = 0;
+        
+        for(int right = 0; right < nums.length; right++) {
+            if(nums[right] == 0) {
+                zeroCount++;
+            }
+
             while(zeroCount > 1) {
                 if(nums[left] == 0) {
                     zeroCount--;
@@ -19,5 +41,6 @@ class Solution {
             answer = Math.max(answer, right - left);
         }
         return answer;
+        */
     }
 }
