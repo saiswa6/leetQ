d36a895c4faf075e978d837523ef5e4abf82a3f8
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-02-11 15:07:32 +0530

    Update SW Pattern Solution.java

--- 1493-longest-subarray-of-1s-after-deleting-one-element/SW Pattern Solution.java
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/SW Pattern Solution.java
@@ -1,5 +1,26 @@
 // Windows Shrinkable Pattern Version
 
+/*Sliding Window (Shrinkable)
+What's state? cnt as the number of 0s in the window.
+What's invalid? cnt > 1 is invalid.*/
+// OJ: https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/
+// Author: github.com/lzl124631x
+// Time: O(N)
+// Space: O(1)
+class Solution {
+public:
+    int longestSubarray(vector<int>& A) {
+        int i = 0, j = 0, N = A.size(), cnt = 0, ans = 0;
+        for (; j < N; ++j) {
+            cnt += A[j] == 0;
+            while (cnt > 1) cnt -= A[i++] == 0;
+            ans = max(ans, j - i); // note that the window is of size `j - i + 1`. We use `j - i` here because we need to delete a number.
+        }
+        return ans;
+    }
+};
+
+// My Soln
 class Solution {
     public int longestSubarray(int[] nums) { 
         int answer = 0;
@@ -23,11 +44,29 @@ class Solution {
         return answer;
     }
 }
+
         
 
 // Windows Non-Shrinkable Pattern Version
+===================================================
+    //Sliding Window (Non-shrinkable)
+// OJ: https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/
+// Author: github.com/lzl124631x
+// Time: O(N)
+// Space: O(1)
+class Solution {
+public:
+    int longestSubarray(vector<int>& A) {
+        int i = 0, j = 0, N = A.size(), cnt = 0;
+        for (; j < N; ++j) {
+            cnt += A[j] == 0;
+            if (cnt > 1) cnt -= A[i++] == 0;
+        }
+        return j - i - 1;
+    }
+};
 
-
+//My Solution
 class Solution {
     public int longestSubarray(int[] nums) {
         int answer = 0;
