b1751c1ffce7e52a36bd1b13776e1c0dd7343dce
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-02-11 14:56:39 +0530

    Update SW Pattern Solution.java

--- 1493-longest-subarray-of-1s-after-deleting-one-element/SW Pattern Solution.java
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/SW Pattern Solution.java
@@ -1,3 +1,33 @@
+// Windows Shrinkable Pattern Version
+
+class Solution {
+    public int longestSubarray(int[] nums) { 
+        int answer = 0;
+        int zeroCount = 0;
+        int left = 0;
+        
+        for(int right = 0; right < nums.length; right++) {
+            if(nums[right] == 0) {
+                zeroCount++;
+            }
+
+            while(zeroCount > 1) {  //-------------------------> Replace wth if to make it non-shrinkable
+                if(nums[left] == 0) {
+                    zeroCount--;
+                }
+                left++;
+            }
+
+            answer = Math.max(answer, right - left);
+        }
+        return answer;
+    }
+}
+        
+
+// Windows Non-Shrinkable Pattern Version
+
+
 class Solution {
     public int longestSubarray(int[] nums) {
         int answer = 0;
@@ -9,7 +39,7 @@ class Solution {
                 zeroCount++;
             }
 
-            if(zeroCount > 1) {
+            if(zeroCount > 1) {   //-------------------------> Replace wth if to make it non-shrinkable
                 if(nums[left] == 0) {
                     zeroCount--;
                 }
@@ -19,28 +49,5 @@ class Solution {
             answer = Math.max(answer, right - left);
         }
         return answer;
-
-
-        /* 
-        int answer = 0;
-        int zeroCount = 0;
-        int left = 0;
-        
-        for(int right = 0; right < nums.length; right++) {
-            if(nums[right] == 0) {
-                zeroCount++;
-            }
-
-            while(zeroCount > 1) {
-                if(nums[left] == 0) {
-                    zeroCount--;
-                }
-                left++;
-            }
-
-            answer = Math.max(answer, right - left);
-        }
-        return answer;
-        */
     }
 }
