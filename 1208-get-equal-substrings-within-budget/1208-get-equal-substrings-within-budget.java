508aab84dc7cb6dd16d427928c0eaec2e7134ca8
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-02-12 21:51:31 +0530

    Time: 12 ms (10.04%), Space: 43.2 MB (55.58%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.java
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.java
@@ -8,6 +8,27 @@ class Solution {
         for( ; right < s.length(); right++) {
             sum += Math.abs(s.charAt(right) - t.charAt(right));
 
+            if(sum > maxCost) {
+                sum -= Math.abs(s.charAt(left) - t.charAt(left));
+                left++;
+            }
+
+            answer = Math.max(right - left + 1, answer);
+        }
+
+        return answer;
+
+
+
+/*
+        int left = 0;
+        int right = 0;
+        int answer = 0;
+        int sum = 0;
+
+        for( ; right < s.length(); right++) {
+            sum += Math.abs(s.charAt(right) - t.charAt(right));
+
             while(sum > maxCost) {
                 sum -= Math.abs(s.charAt(left) - t.charAt(left));
                 left++;
@@ -17,5 +38,7 @@ class Solution {
         }
 
         return answer;
+ */
+
     }
 }
