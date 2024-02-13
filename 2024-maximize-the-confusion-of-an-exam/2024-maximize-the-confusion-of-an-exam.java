b2f5d1401dde7e89189a8694995c37f44e4ea4df
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-02-13 07:25:09 +0530

    Time: 12 ms (87.32%), Space: 44.9 MB (38.05%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -16,6 +16,40 @@ class Solution {
                 numberTrue++;
             }
 
+            if(numberFalse > k) {
+                if(answerKey.charAt(fLeft) == 'F') {
+                    numberFalse--;
+                }
+                fLeft++;
+            }
+            if(numberTrue > k) {
+                if(answerKey.charAt(tLeft) == 'T') {
+                    numberTrue--;
+                }
+                tLeft++;
+            }
+
+        }
+        return Math.max(right - fLeft , right - tLeft);
+
+
+        /*
+        int fLeft = 0;
+        int tLeft = 0;
+        int right = 0;
+        int numberFalse = 0;
+        int numberTrue = 0;
+        int answer = 0;
+
+        int length = answerKey.length();
+
+        for( ; right < length ; right++ ) {
+            if(answerKey.charAt(right) == 'F') {
+                numberFalse++;
+            } else if(answerKey.charAt(right) == 'T') {
+                numberTrue++;
+            }
+
             while(numberFalse > k) {
                 if(answerKey.charAt(fLeft) == 'F') {
                     numberFalse--;
@@ -32,6 +66,7 @@ class Solution {
             answer = Math.max(answer, Math.max(right - fLeft + 1, right - tLeft + 1));
         }
         return answer;
+         */
 
     }
 }
