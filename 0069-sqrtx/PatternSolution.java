7e398c3d223fdc76d1d912191deaec260b947260
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-02-19 19:20:36 +0530

    Update and rename 0069-sqrtx.java to PatternSolution.java

--- 0069-sqrtx/PatternSolution.java
+++ 0069-sqrtx/PatternSolution.java
@@ -1,3 +1,5 @@
+//Easy one. First we need to search for minimal k satisfying condition k^2 > x, then k - 1 is the answer to the question. We can easily come up with the solution. Notice that I set right = x + 1 instead of right = x to deal with special input cases like x = 0 and x = 1.
+
 class Solution {
     public int mySqrt(int x) {
         long left = 0, right = (long)x + 1; // when x is 2147483647, then addition of 1 to INT_MAX oveflows to -2147483648. So, convert to long.
