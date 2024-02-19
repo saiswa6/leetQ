ee68f1deaf757b4bf6912bd12898b1b8eeaf3a5c
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-02-19 18:26:32 +0530

    Time: 2 ms (20.49%), Space: 40.8 MB (61.38%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,16 +1,16 @@
 class Solution {
     public int mySqrt(int x) {
-        long left = 0, right = (long)x + 1;
+        long left = 0, right = (long)x + 1; // when x is 2147483647, then addition of 1 to INT_MAX oveflows to -2147483648. So, convert to long.
 
         while(left < right) {
             long mid = left + ((right - left)/2);
 
-            if((long)mid*mid > x) {
+            if((long)mid*mid > x) { // overflows int * int, use type cast to long.
                 right = mid;
             } else {
                 left = mid + 1;
             }
         }
-        return (int)left - 1;
+        return (int)left - 1; // back type cast to retun answer in int.
     }
 }
