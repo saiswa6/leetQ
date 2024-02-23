2854e63249b72399275f55c3d74f25953a3378ee
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-02-23 10:05:23 +0530

    Update and rename 2330-valid-palindrome-iv.java to MySolution.java

--- 2330-valid-palindrome-iv/MySolution.java
+++ 2330-valid-palindrome-iv/MySolution.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean makePalindrome(String s) {
         int start = 0;
         int end = s.length() - 1;
-        int count = 2;
+        int count = 2; // As max is 2 operations, check for max 2 cases where start and end char matches. 
 
         while(start < end) {
             if(s.charAt(start) != s.charAt(end)) {
