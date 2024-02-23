192abbfbe73e69dcd95ee83603a841c787cf442e
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-02-23 08:34:01 +0530

    Update and rename 0680-valid-palindrome-ii.java to LearnedSolution.java

--- 0680-valid-palindrome-ii/LearnedSolution.java
+++ 0680-valid-palindrome-ii/LearnedSolution.java
@@ -1,3 +1,52 @@
+// Below is my solution. This failes for test case : "aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga"
+// When it is extracted to  "cupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupucu" this, I checked start, end - 1 only, it is true, but later fails
+// Ideally, start + 1, end suits here. So, we need to check both variations and confirm.
+
+class Solution {
+    public boolean validPalindrome(String s) {
+
+        char sarray [] = s.toCharArray();
+        int start = 0;
+        int end = s.length() - 1;
+        char startChar = '&';
+        char startChar2 = '&';
+        char endChar = '&';
+        char endChar2 = '&';
+        boolean isDeleteOne = false;
+        while (start < end) {
+            startChar = sarray[start];
+            endChar = sarray[end];
+            if(sarray[start] == sarray[end]) {
+                start++;
+                end--;
+                continue;
+            }
+            else if (sarray[start] != sarray[end]) {
+                if (isDeleteOne) {
+                    return false;
+                }  else if (sarray[start] == sarray[end - 1]) {
+                    endChar = sarray[end ];
+                    endChar2 = sarray[end - 1];
+                    startChar = sarray[start];
+                    isDeleteOne = true;
+                    end--;
+                } else if (sarray[start + 1] == sarray[end]) {
+                    startChar = sarray[start];
+                    startChar2 = sarray[start + 1];
+                    endChar = sarray[end];
+                    isDeleteOne = true;
+                    start ++;
+                }else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+//////////////////////////////////////////////////////////
+    // Learned Solution
 class Solution {
     public boolean validPalindrome(String s) {
 
