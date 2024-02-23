56993058876ec49592b6883b8cdb42c415bedc1a
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-02-23 10:15:07 +0530

    Update MySolution.java

--- 1216-valid-palindrome-iii/MySolution.java
+++ 1216-valid-palindrome-iii/MySolution.java
@@ -20,13 +20,13 @@ class Solution {
 
     public boolean checkPalindrome(String s, int start, int end, int count) { // Here recursively check for count times deleting , whether it becomes palindrome.
         while(start < end) {
-            if(s.charAt(start) == s.charAt(end)) {
+            if(s.charAt(start) == s.charAt(end)) { // If equal, increment
                 start++;
                 end--;
             } else if(s.charAt(start) != s.charAt(end) & count > 0) {
-                return (checkPalindrome(s, start + 1, end, count - 1) || checkPalindrome(s, start , end - 1, count - 1)) ;
+                return (checkPalindrome(s, start + 1, end, count - 1) || checkPalindrome(s, start , end - 1, count - 1)) ; // check for both cases.
             } else {
-                return false;
+                return false; // If count exhausted, return false
             }
         }
         return true;
