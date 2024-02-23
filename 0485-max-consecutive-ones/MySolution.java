9613755efbedeeb5f781d70a433537beb6049629
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-02-23 13:32:47 +0530

    Update and rename 0485-max-consecutive-ones.java to MySolution.java

--- 0485-max-consecutive-ones/MySolution.java
+++ 0485-max-consecutive-ones/MySolution.java
@@ -7,10 +7,10 @@ class Solution {
 
         for(int i = 0; i < length; i++) {
             if(nums[i] == 1) {
-                if(i == 0 || nums[i-1] == 0) {
+                if(i == 0 || nums[i-1] == 0) { // if element is starting one or number 1 just after 0, point it as start.
                     start = i;
                 }
-                if(i == length - 1 || nums[i + 1] == 0) {
+                if(i == length - 1 || nums[i + 1] == 0) { // if element is last one one or number 1 just before 0, point it as end and calculate window length.
                     end = i;
                     ans = Math.max(ans, end - start + 1);
                 }
