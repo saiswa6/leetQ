f194b2d7bcdfce4a63baa880f09d13209d0e75b7
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-02-23 16:12:42 +0530

    Update LC2.java

--- 0121-best-time-to-buy-and-sell-stock/LC2.java
+++ 0121-best-time-to-buy-and-sell-stock/LC2.java
@@ -6,8 +6,10 @@ Say the given array is:
 
 If we plot the numbers of the given array on a graph, we get:
 
-The points of interest are the peaks and valleys in the given graph. We need to find the largest price following each valley, which difference could be the max profit.
-We can maintain two variables - minprice and maxprofit corresponding to the smallest valley and maximum profit (maximum difference between selling price and minprice) obtained so far respectively.
+- The points of interest are the peaks and valleys in the given graph. We need to find the largest price following each valley, which difference could be the max profit.
+- We can maintain two variables - minprice and maxprofit corresponding to the smallest valley and maximum profit (maximum difference between selling price and minprice) obtained so far respectively.
+- The sentence "We need to find the largest peak following the smallest valley" is plain wrong. What we are actually doing is this: for every element, we are calculating the difference between that element and the minimum of all the values before that element and we are updating the maximum profit if the difference thus found is greater than the current maximum profit.
+- The logic to solve this problem is same as "max subarray problem" using Kadane's Algorithm
 */
 public class Solution {
     public int maxProfit(int prices[]) {
