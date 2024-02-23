92755be42ab786ccd094a911c03c42a66948f737
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-02-23 10:14:10 +0530

    Update and rename 1216-valid-palindrome-iii.java to MySolution.java

---
+++ 1216-valid-palindrome-iii/MySolution.java
@@ -0,0 +1,34 @@
+// My Solution works for low k numbers. It fails when k is 216. DP needed here. When DP is learnt, I will solve this again.
+
+class Solution {
+
+
+    public boolean isValidPalindrome(String s, int k) {
+        int start = 0;
+        int end = s.length() - 1;
+        int count = k;
+
+        while(start < end) {
+            if(s.charAt(start) != s.charAt(end) && count > 0) {
+                return checkPalindrome(s, start, end, count); // call subfunction
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public boolean checkPalindrome(String s, int start, int end, int count) { // Here recursively check for count times deleting , whether it becomes palindrome.
+        while(start < end) {
+            if(s.charAt(start) == s.charAt(end)) {
+                start++;
+                end--;
+            } else if(s.charAt(start) != s.charAt(end) & count > 0) {
+                return (checkPalindrome(s, start + 1, end, count - 1) || checkPalindrome(s, start , end - 1, count - 1)) ;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
