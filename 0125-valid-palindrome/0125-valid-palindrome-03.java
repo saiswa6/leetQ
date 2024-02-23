b7b6b337d4267a353ae5471d7703eedbc915ce35
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-02-23 06:40:51 +0530

    Update 0125-valid-palindrome-03.java

--- 0125-valid-palindrome/0125-valid-palindrome-03.java
+++ 0125-valid-palindrome/0125-valid-palindrome-03.java
@@ -1,23 +1,23 @@
 /*
 Approach 2: Two Pointers
 Intuition
-If you take any ordinary string, and concatenate its reverse to it, you'll get a palindrome. This leads to an interesting insight about the converse: every palindrome half is reverse of the other half.
-Simply speaking, if one were to start in the middle of a palindrome, and traverse outwards, they'd encounter the same characters, in the exact same order, in both halves!
+- If you take any ordinary string, and concatenate its reverse to it, you'll get a palindrome. This leads to an interesting insight about the converse: every palindrome half is reverse of the other half.
+- Simply speaking, if one were to start in the middle of a palindrome, and traverse outwards, they'd encounter the same characters, in the exact same order, in both halves!
 
 Algorithm
-Since the input string contains characters that we need to ignore in our palindromic check, it becomes tedious to figure out the real middle point of our palindromic input.
-Instead of going outwards from the middle, we could just go inwards towards the middle!
-So, if we start traversing inwards, from both ends of the input string, we can expect to see the same characters, in the same order.
+- Since the input string contains characters that we need to ignore in our palindromic check, it becomes tedious to figure out the real middle point of our palindromic input.
+- Instead of going outwards from the middle, we could just go inwards towards the middle!
+  So, if we start traversing inwards, from both ends of the input string, we can expect to see the same characters, in the same order.
 
 The resulting algorithm is simple:
-Set two pointers, one at each end of the input string
-If the input is palindromic, both the pointers should point to equivalent characters, at all times. 
-- If this condition is not met at any point of time, we break and return early. 2
-We can simply ignore non-alphanumeric characters by continuing to traverse further.
-Continue traversing inwards until the pointers meet in the middle.
+- Set two pointers, one at each end of the input string
+  If the input is palindromic, both the pointers should point to equivalent characters, at all times. 
+- If this condition is not met at any point of time, we break and return early. 
+  We can simply ignore non-alphanumeric characters by continuing to traverse further.
+- Continue traversing inwards until the pointers meet in the middle.
 
 Complexity Analysis
-Time complexity : O(n), in length nnn of the string. We traverse over each character at-most once, until the two pointers meet in the middle, or when we break and return early.
+Time complexity : O(n), in length n of the string. We traverse over each character at-most once, until the two pointers meet in the middle, or when we break and return early.
 Space complexity : O(1). No extra space required, at all.
 
 Footnotes
@@ -63,9 +63,10 @@ class Solution {
             continue;
         }
         
-        if(Character.toLowerCase(cb) != Character.toLowerCase(c)) return false;
-        p++;
-        q--;
+        if(Character.toLowerCase(cb) != Character.toLowerCase(c)) 
+          return false;
+        p++; // V. Imp
+        q--; // V. Imp
     }
     return true;
     
