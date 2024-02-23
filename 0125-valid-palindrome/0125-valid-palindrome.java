2052c80cff9b702cd60009144c6beaebd3216719
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-02-23 06:54:51 +0530

    Time: 2 ms (99.08%), Space: 42.6 MB (95.05%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,6 +1,31 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int start = 0;
+        if(s.isEmpty()) {
+            return true;
+        }
+        int left = 0;
+        int right = s.length() - 1;
+
+        while(left < right) {
+            char l = s.charAt(left);
+            char r = s.charAt(right);
+
+            if(!Character.isLetterOrDigit(l)) {
+                left++;
+            } else if(!Character.isLetterOrDigit(r)) {
+                right--;
+            } else {
+                if(Character.toLowerCase(l) != Character.toLowerCase(r)) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+        }
+        return true;
+
+
+        /*int start = 0;
         int end = s.length() - 1;
 
         while(start < end) {
@@ -17,7 +42,7 @@ class Solution {
             start++;
             end--;
         }
-        return true;
+        return true;*/
     }
 
     public boolean isAlphaNumeric(char ch) {
