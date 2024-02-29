615033fb6521804c6af8a8e5057d1ae009500fa0
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-02-29 21:14:46 +0530

    Update PatternSolution.java

--- 0046-permutations/PatternSolution.java
+++ 0046-permutations/PatternSolution.java
@@ -42,3 +42,38 @@ private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] 
       }
    }
 } 
+
+
+/*
+- Good but tmpList.contains(nums[i]) is a O(N) operation
+- I suggest adding a hashset for checking if nums[i] exist in the tmp number list.
+- In this way, tmpSet.contains(nums[i]) only costs O(1). The algorithm will be faster in the long run.
+- The space complexity should remain the same.
+*/
+
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        permuteCalc(new ArrayList<Integer>(), new HashSet<>(),nums, ans);
+        return ans;
+    }
+
+    public void permuteCalc(List<Integer> processed, Set<Integer> hs,int nums[], List<List<Integer>> ans) {
+        if(processed.size() == nums.length) {
+            ans.add(new ArrayList<>(processed));
+            return;
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            if(hs.contains(nums[i])) {
+                continue;
+            }
+            processed.add(nums[i]);
+            hs.add(nums[i]);
+            permuteCalc(processed, hs, nums, ans);
+            int lastElement = processed.get(processed.size() - 1);
+            hs.remove(lastElement);
+            processed.remove(processed.size() - 1);
+        }
+    }
+}
