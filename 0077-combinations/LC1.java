4c18197449ee7733822699e14eb570f99674c206
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-03-01 09:40:55 +0530

    Create LC1.java

---
+++ 0077-combinations/LC1.java
@@ -0,0 +1,32 @@
+// Read th editorial and TC , SC to understand and note down imp points
+class Solution {
+    private int n;
+    private int k;
+    
+    public List<List<Integer>> combine(int n, int k) {
+        this.n = n;
+        this.k = k;
+        List<List<Integer>> ans = new ArrayList<>();
+        backtrack(new ArrayList<>(), 1, ans);
+        return ans;
+    }
+    
+    public void backtrack(List<Integer> curr, int firstNum, List<List<Integer>> ans) {
+        if (curr.size() == k) {
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+        
+        int need = k - curr.size();
+        int remain = n - firstNum + 1;
+        int available = remain - need;
+        
+        for (int num = firstNum; num <= firstNum + available; num++) {
+            curr.add(num);
+            backtrack(curr, num + 1, ans);
+            curr.remove(curr.size() - 1);
+        }
+
+        return;
+    }
+}
