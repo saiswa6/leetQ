eced0e212620005d2ccb5d022eb044aa2d21434d
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-03-01 08:44:59 +0530

    Time: 3 ms (86.64%), Space: 42.9 MB (86.77%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> answer = new ArrayList<>();
+        Arrays.sort(candidates); //--> No need of sorting
+        combo(candidates, target, answer,0, new ArrayList<Integer>(), 0);
+        return answer;
+    }
+
+    void combo(int[] candidates, int target,List<List<Integer>> answer, int sum , List<Integer> processed, int index) {
+        if(sum > target){
+            return;
+        } else if(sum == target) {
+            answer.add(new ArrayList<>(processed));
+            return;
+        } else {
+            for(int i = index; i < candidates.length; i++) {
+                if(i > index && candidates[i] == candidates[i-1]) {
+                    continue;
+                }
+                if(target - sum < candidates[i]) {
+                    break;
+                }
+                sum = sum + candidates[i];
+                processed.add(candidates[i]);
+                combo(candidates, target, answer, sum, processed,i + 1);
+                sum = sum - candidates[i];
+                processed.remove(processed.size() - 1);
+            }
+        }
+        
+    }
+}
