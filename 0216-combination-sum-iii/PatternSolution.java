4866a97a6928324bfb919dc971740f5441814284
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-03-01 09:07:55 +0530

    Update and rename 0216-combination-sum-iii.java to PatternSolution.java

--- 0216-combination-sum-iii/PatternSolution.java
+++ 0216-combination-sum-iii/PatternSolution.java
@@ -1,21 +1,20 @@
 class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> answer = new ArrayList<>();
-        int candidates [] = {1,2,3,4,5,6,7,8,9};
-        //Arrays.sort(candidates); 
+        int candidates [] = {1,2,3,4,5,6,7,8,9}; // 1st diff
         combo(candidates, n, answer, 0, new ArrayList<Integer>(), 0, k);
         return answer;
     }
 
     void combo(int[] candidates, int target,List<List<Integer>> answer, int sum , List<Integer> processed, int index, int digits) {
-        if(sum > target || processed.size() > digits){
+        if(sum > target || processed.size() > digits){ // 2nd diff - Imp - sum > target and if digits are more, return
             return;
-        } else if(sum == target & processed.size() == digits) {
+        } else if(sum == target & processed.size() == digits) { // 3rd diff 
             answer.add(new ArrayList<>(processed));
             return;
         } else {
             for(int i = index; i < candidates.length; i++) {
-                if(target-sum < candidates[i]) {
+                if(target-sum < candidates[i]) {  // 4th diff - if candidate[i] greater than target - sum.
                     break;
                 }
                 sum = sum + candidates[i];
