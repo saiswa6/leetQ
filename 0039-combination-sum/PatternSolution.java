370af928679d26d7194ee5c16086274820793211
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-03-01 08:16:00 +0530

    Update and rename 0039-combination-sum.java to PatternSolution.java

--- 0039-combination-sum/PatternSolution.java
+++ 0039-combination-sum/PatternSolution.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> answer = new ArrayList<>();
-
+        Arrays.sort(nums);
         combo(candidates, target, answer,0, new ArrayList<Integer>(), 0);
         return answer;
     }
@@ -22,5 +22,25 @@ class Solution {
             }
         }
         
+    }
+}
+
+// DIrect copy pattern 
+public List<List<Integer>> combinationSum(int[] nums, int target) {
+    List<List<Integer>> list = new ArrayList<>();
+    Arrays.sort(nums);
+    backtrack(list, new ArrayList<>(), nums, target, 0);
+    return list;
+}
+
+private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
+    if(remain < 0) return;
+    else if(remain == 0) list.add(new ArrayList<>(tempList));
+    else{ 
+        for(int i = start; i < nums.length; i++){
+            tempList.add(nums[i]);
+            backtrack(list, tempList, nums, remain - nums[i], i); // not i + 1 because we can reuse same elements
+            tempList.remove(tempList.size() - 1);
+        }
     }
 }
