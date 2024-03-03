55626fdc67358e6a489873b63544a69e6ac44844
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-03-03 12:10:27 +0530

    Update and rename 0116-populating-next-right-pointers-in-each-node.java to KunalPatternSolution.java

--- 0116-populating-next-right-pointers-in-each-node/KunalPatternSolution.java
+++ 0116-populating-next-right-pointers-in-each-node/KunalPatternSolution.java
@@ -21,24 +21,26 @@ class Node {
 };
 */
 
+// Do solution like Linked list
+
 class Solution {
     public Node connect(Node root) {
         if(root == null) {
             return root;
         }
-        Node leftMost = root;
+        Node leftMost = root; // Always take leftmost onw as it is a start of each level
 
         while(leftMost.left != null) {
             Node current = leftMost;
 
             while(current != null) {
-                current.left.next = current.right;
+                current.left.next = current.right; //Link left node with right node
                 if(current.next != null) {
-                    current.right.next = current.next.left;
+                    current.right.next = current.next.left; // Link right node with left node of next current node
                 }
-                current = current.next;
+                current = current.next; // move forward
             }
-            leftMost = leftMost.left;
+            leftMost = leftMost.left; // Go leftmost of next level
         }
         return root;
     }
