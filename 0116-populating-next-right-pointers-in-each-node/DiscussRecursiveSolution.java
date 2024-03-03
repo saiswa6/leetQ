43a9bf31872354eee3748b610eba4763324ee27c
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-03-03 12:32:06 +0530

    Update DiscussRecursiveSolution.java

--- 0116-populating-next-right-pointers-in-each-node/DiscussRecursiveSolution.java
+++ 0116-populating-next-right-pointers-in-each-node/DiscussRecursiveSolution.java
@@ -33,3 +33,20 @@ var connect = function(root) {
     connect(root.right)
     return root
 };
+
+// 3rd
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        if(root == null || root.left == null) return;
+        connectNodes(root.left, root.right);
+    }
+    
+    public void connectNodes(TreeLinkNode node1, TreeLinkNode node2) {
+        node1.next = node2;
+        if(node1.left != null) {
+            connectNodes(node1.right, node2.left);
+            connectNodes(node1.left, node1.right);
+            connectNodes(node2.left, node2.right);
+        }
+    } 
+}
