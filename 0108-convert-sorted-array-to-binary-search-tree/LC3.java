1715acbc8aa09b4fb5336d0a8e9f4ff12bf2d620
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-03-04 12:15:19 +0530

    Create LC3.java

---
+++ 0108-convert-sorted-array-to-binary-search-tree/LC3.java
@@ -0,0 +1,44 @@
+/*
+Approach 3: Preorder Traversal: Choose a Random Middle Node as a Root
+This one is for fun. Instead of a predefined choice, we will pick randomly the left or right middle node at each step. Each run will result in a different solution and they all will be accepted.
+
+Algorithm
+- Implement helper function helper(left, right), which constructs BST from nums elements between indexes left and right:
+  - If left > right, then there are no elements available for that subtree. Return None.
+  - Pick a random middle element:
+     - p = (left + right) // 2.
+     - If left + right is odd, add randomly 0 or 1 to p-index.
+  - Initiate the root: root = TreeNode(nums[p]).
+  - Compute recursively left and right subtrees: root.left = helper(left, p - 1), root.right = helper(p + 1, right).
+- Return helper(0, len(nums) - 1).
+  */
+
+class Solution {
+    int[] nums;
+    Random rand = new Random();
+    
+    public TreeNode helper(int left, int right) {
+        if (left > right) return null;
+        
+        // choose random middle node as a root
+        int p = (left + right) / 2; 
+        if ((left + right) % 2 == 1) p += rand.nextInt(2);
+
+        // preorder traversal: node -> left -> right
+        TreeNode root = new TreeNode(nums[p]);
+        root.left = helper(left, p - 1);
+        root.right = helper(p + 1, right);
+        return root;
+    }
+    
+    public TreeNode sortedArrayToBST(int[] nums) {
+        this.nums = nums;
+        return helper(0, nums.length - 1);    
+    }
+}
+/*
+Complexity Analysis
+Time complexity: O(N) since we visit each node exactly once.
+Space complexity: O(log⁡N)
+The recursion stack requires O(log⁡N) space because the tree is height-balanced. Note that the O(N) space used to store the output does not count as auxiliary space, so it is not included in the space complexity.
+  */
