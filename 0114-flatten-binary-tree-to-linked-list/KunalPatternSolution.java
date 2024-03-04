9b91b3821ebc02f2367d2f40caab5f2e63347e59
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-03-04 13:13:30 +0530

    Update and rename 0114-flatten-binary-tree-to-linked-list.java to KunalPatternSolution.java

--- 0114-flatten-binary-tree-to-linked-list/KunalPatternSolution.java
+++ 0114-flatten-binary-tree-to-linked-list/KunalPatternSolution.java
@@ -13,6 +13,10 @@
  *     }
  * }
  */
+
+//Refer Kunal Pattern Diagram
+// Check if current node has a left child. If yes, find the rightmost element of it. Attach the current right to it. Now, convert the left child of current to right child of current and make left of current as NULL.
+
 class Solution {
     public void flatten(TreeNode root) {
         TreeNode current = root;
