6a8066e4e6ee510ee91a7e9459fdda01a8f33e9e
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-03-04 10:55:59 +0530

    Create Discuss.java

---
+++ 0226-invert-binary-tree/Discuss.java
@@ -0,0 +1,29 @@
+//Using stack similar to recursive solution as it use functional stack
+The above solution is correct, but it is also bound to the application stack, which means that it's no so much scalable - (you can find the problem size that will overflow the stack and crash your application), so more robust solution would be to use stack data structure.
+
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        
+        if (root == null) {
+            return null;
+        }
+
+        final Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+        
+        while(!stack.isEmpty()) {
+            final TreeNode node = stack.pop();
+            final TreeNode left = node.left;
+            node.left = node.right;
+            node.right = left;
+            
+            if(node.left != null) {
+                stack.push(node.left);
+            }
+            if(node.right != null) {
+                stack.push(node.right);
+            }
+        }
+        return root;
+    }
+}
