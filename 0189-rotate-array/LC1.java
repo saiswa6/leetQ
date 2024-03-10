d69e3487c23c4e8093f2e8ca7694aa733949e9d2
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-03-10 17:50:49 +0530

    Update LC1.java

--- 0189-rotate-array/LC1.java
+++ 0189-rotate-array/LC1.java
@@ -1,5 +1,5 @@
 /*Approach 1: Brute Force
-The simplest approach is to rotate all the elements of the array in kkk steps
+The simplest approach is to rotate all the elements of the array in k steps
 by rotating the elements by 1 unit in each step.
 
 Complexity Analysis
