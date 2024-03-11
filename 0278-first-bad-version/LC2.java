04156c4b9e5ca139d18c3a4007224568b75c5dac
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-03-11 08:18:14 +0530

    Update LC2.java

--- 0278-first-bad-version/LC2.java
+++ 0278-first-bad-version/LC2.java
@@ -8,7 +8,7 @@ Scenario #1: isBadVersion(mid) => false
  G G G G G G B B B       G = Good, B = Bad
  |       |       |
 left    mid    right
-Let us look at the first scenario above where isBadVersion(mid)⇒falseisBadVersion(mid) \Rightarrow falseisBadVersion(mid)⇒false. We know that all versions preceding and including midmidmid are all good. So we set left=mid+1 to indicate that the new search space is the interval [mid+1,right][mid + 1, right][mid+1,right] (inclusive).
+Let us look at the first scenario above where isBadVersion(mid)⇒false. We know that all versions preceding and including midmidmid are all good. So we set left=mid+1 to indicate that the new search space is the interval [mid+1,right] (inclusive).
 
 Scenario #2: isBadVersion(mid) => true
 ------------------------------------------------------
@@ -16,8 +16,8 @@ Scenario #2: isBadVersion(mid) => true
  G G G B B B B B B       G = Good, B = Bad
  |       |       |
 left    mid    right
-- The only scenario left is where isBadVersion(mid)⇒true. This tells us that midmidmid may or may not be the first bad version, but we can tell for sure that all versions after midmidmid can be discarded. Therefore we set right=mid as the new search space of interval [left,mid] (inclusive).
-- In our case, we indicate leftleftleft and rightrightright as the boundary of our search space (both inclusive). This is why we initialize left=1 and right=n. How about the terminating condition? We could guess that leftleftleft and right eventually both meet and it must be the first bad version, but how could you tell for sure?
+- The only scenario left is where isBadVersion(mid)⇒true. This tells us that mid may or may not be the first bad version, but we can tell for sure that all versions after midmidmid can be discarded. Therefore we set right=mid as the new search space of interval [left,mid] (inclusive).
+- In our case, we indicate left and right as the boundary of our search space (both inclusive). This is why we initialize left=1 and right=n. How about the terminating condition? We could guess that left and right eventually both meet and it must be the first bad version, but how could you tell for sure?
 - The formal way is to prove by induction, which you can read up yourself if you are interested. Here is a helpful tip to quickly prove the correctness of your binary search algorithm
 during an interview. We just need to test an input of size 2. Check if it reduces the search space to a single element (which must be the answer) for both of the scenarios above. If not, your algorithm will never terminate.
 - If you are setting mid=left+right/2 , you have to be very careful. Unless you are using a language that does not overflow such as Python, left+right could overflow. One way to fix this is to use left+(right−left)/2 instead.
