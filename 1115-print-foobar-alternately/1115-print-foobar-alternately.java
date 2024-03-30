a3d55a1b9066840b10d2748f6644d0d3362a7bbf
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-03-30 10:57:41 +0530

    Time: 24 ms (56.9%), Space: 42.7 MB (99.59%) - LeetHub

---
+++ 1115-print-foobar-alternately/1115-print-foobar-alternately.java
@@ -0,0 +1,36 @@
+class FooBar {
+    private int n;
+    boolean isFoo;
+
+    public FooBar(int n) {
+        this.n = n;
+        isFoo = true;
+    }
+
+    public synchronized void foo(Runnable printFoo) throws InterruptedException {
+        
+        for (int i = 0; i < n; i++) {
+            while(!isFoo){
+                wait();
+            }
+        	// printFoo.run() outputs "foo". Do not change or remove this line.
+        	printFoo.run();
+            isFoo = false;
+            notify();
+        }
+    }
+
+    public synchronized void bar(Runnable printBar) throws InterruptedException {
+        
+        for (int i = 0; i < n; i++) {
+            while (isFoo) {
+                wait();
+            }
+            
+            // printBar.run() outputs "bar". Do not change or remove this line.
+        	printBar.run();
+            isFoo = true;
+            notify();
+        }
+    }
+}
