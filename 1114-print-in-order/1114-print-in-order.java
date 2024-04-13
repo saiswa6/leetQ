455b857b6b62eb4112fe0269dd4f2a8704ecc09b
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-04-13 11:25:50 +0530

    Time: 11 ms (88.24%), Space: 41.9 MB (72.91%) - LeetHub

--- 1114-print-in-order/1114-print-in-order.java
+++ 1114-print-in-order/1114-print-in-order.java
@@ -1,38 +1,56 @@
 class Foo {
-
+    ReentrantLock lock ;
+    Condition firstDone ;
+    Condition secondDone;
     private boolean isFirstDone;
     private boolean isSecondDone;
 
     public Foo() {
+        lock = new ReentrantLock();
+        firstDone = lock.newCondition();
+        secondDone = lock.newCondition();
         isFirstDone = false;
         isSecondDone = false;
     }
 
-    public synchronized void first(Runnable printFirst) throws InterruptedException {
-        
+    public void first(Runnable printFirst) throws InterruptedException {
+        try{
+            lock.lock();
         // printFirst.run() outputs "first". Do not change or remove this line.
         printFirst.run();
         isFirstDone = true;
-        notifyAll();
+        firstDone.signalAll();
+        } finally {
+            lock.unlock();
+        }
     }
 
-    public synchronized void second(Runnable printSecond) throws InterruptedException {
-        while(!isFirstDone){
-            wait();
-        }
-        
+    public void second(Runnable printSecond) throws InterruptedException {
+        try{
+            lock.lock();
+            while(!isFirstDone) {
+                firstDone.await();
+            }
         // printSecond.run() outputs "second". Do not change or remove this line.
         printSecond.run();
         isSecondDone = true;
-        notifyAll();
+        secondDone.signalAll();
+        } finally{
+            lock.unlock();
+        }
     }
 
-    public synchronized void third(Runnable printThird) throws InterruptedException {
-        while (!isFirstDone || !isSecondDone) {
-            wait();
-        }
+    public void third(Runnable printThird) throws InterruptedException {
+        try{
+            lock.lock();
+            while(!isSecondDone) {
+                secondDone.await();
+            }
         // printThird.run() outputs "third". Do not change or remove this line.
         printThird.run();
-        notifyAll();
+        secondDone.signalAll();
+        } finally{
+            lock.unlock();
+        }
     }
 }
