4e35623a9e5c54f5238c1490c18b423304256f49
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-04-13 13:33:03 +0530

    Time: 7 ms (57.37%), Space: 42.1 MB (36.54%) - LeetHub

--- 1116-print-zero-even-odd/1116-print-zero-even-odd.java
+++ 1116-print-zero-even-odd/1116-print-zero-even-odd.java
@@ -1,48 +1,73 @@
 class ZeroEvenOdd {
     private int n;
-    // private Semaphore
     private volatile int status = 0;
+    private ReentrantLock lock;
+    private Condition zero;
+    private Condition odd;
+    private Condition even;
 
     public ZeroEvenOdd(int n) {
         this.n = n;
+        this.lock = new ReentrantLock();
+        this.zero = lock.newCondition();
+        this.odd = lock.newCondition();
+        this.even = lock.newCondition();
     }
 
     // printNumber.accept(x) outputs "x", where x is an integer.
-    public synchronized void zero(IntConsumer printNumber) throws InterruptedException {
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+        try {
 
-        for (int i = 1; i <= n; i++) {
-            while (status != 0) {      //Put this while block for status inside for loop to maintain context of i = 0, 1, 2, 3,....
-                wait();
+            lock.lock();
+            for (int i = 1; i <= n; i++) {
+                while (status != 0) { // Put this while block for status inside for loop to maintain context of i = 0,
+                                      // 1, 2, 3,....
+                    zero.await();
+                }
+                printNumber.accept(0);
+                status = (i % 2) + 1;
+                if (status == 1) {
+                    even.signalAll();
+                } else {
+                    odd.signalAll();
+                }
             }
-            printNumber.accept(0);
-            status = (i % 2) + 1;
-            notifyAll();
+        } finally {
+            lock.unlock();
         }
 
     }
 
-    public synchronized void even(IntConsumer printNumber) throws InterruptedException {
-
-        for (int i = 2; i <= n; i = i + 2) {
-            while (status != 1) {
-                wait();
+    public void even(IntConsumer printNumber) throws InterruptedException {
+        try {
+            lock.lock();
+            for (int i = 2; i <= n; i = i + 2) {
+                while (status != 1) {
+                    even.await();
+                }
+                printNumber.accept(i);
+                status = 0;
+                zero.signalAll();
             }
-            printNumber.accept(i);
-            status = 0;
-            notifyAll();
+        } finally {
+            lock.unlock();
         }
 
     }
 
-    public synchronized void odd(IntConsumer printNumber) throws InterruptedException {
-
-        for (int i = 1; i <= n; i = i + 2) {
-            while (status != 2) {
-                wait();
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+        try {
+            lock.lock();
+            for (int i = 1; i <= n; i = i + 2) {
+                while (status != 2) {
+                    odd.await();
+                }
+                printNumber.accept(i);
+                status = 0;
+                zero.signalAll();
             }
-            printNumber.accept(i);
-            status = 0;
-            notifyAll();
+        } finally {
+            lock.unlock();
         }
     }
 }
