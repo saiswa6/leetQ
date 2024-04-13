8a154c78667a7691214f5119ff3afb780b41e006
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-04-13 09:45:14 +0530

    Update BestSynchronizedSOlution.java

--- 1117-building-h2o/BestSynchronizedSOlution.java
+++ 1117-building-h2o/BestSynchronizedSOlution.java
@@ -1,40 +1,43 @@
 class H2O {
-    private int hydrogenCount ;
-    private int oxygenCount ;
+    private int hydrogenCount;
+    private int oxygenCount;
 
     public H2O() {
         hydrogenCount = 0;
         oxygenCount = 0;
-        
     }
 
     public synchronized void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
-        if(hydrogenCount == 2 && oxygenCount == 1 ) {
-            hydrogenCount = 0;
-            oxygenCount = 0;
+        while (hydrogenCount == 2 && oxygenCount == 0) {
+            wait();
         }
-		if(hydrogenCount < 2) {
+        
         // releaseHydrogen.run() outputs "H". Do not change or remove this line.
         releaseHydrogen.run();
         hydrogenCount++;
-          notifyAll();
-        } else {
-           wait();
-        }
-    }
 
-    public synchronized void oxygen(Runnable releaseOxygen) throws InterruptedException {
-        if(hydrogenCount == 2 && oxygenCount == 1 ) {
+        if (hydrogenCount == 2 && oxygenCount == 1) {
             hydrogenCount = 0;
             oxygenCount = 0;
         }
-        if(oxygenCount < 1) {
-        // releaseOxygen.run() outputs "O". Do not change or remove this line.
-		releaseOxygen.run();
-        oxygenCount++;
+
         notifyAll();
-        } else {
+    }
+
+    public synchronized void oxygen(Runnable releaseOxygen) throws InterruptedException {
+        while (oxygenCount == 1 && hydrogenCount != 2) {
             wait();
         }
+        
+        // releaseOxygen.run() outputs "O". Do not change or remove this line.
+        releaseOxygen.run();
+        oxygenCount++;
+
+        if (hydrogenCount == 2 && oxygenCount == 1) {
+            hydrogenCount = 0;
+            oxygenCount = 0;
+        }
+
+        notifyAll();
     }
 }
