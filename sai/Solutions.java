4b15364ee1336be5e8d613108167dfea26455c63
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-04-13 12:11:23 +0530

    Update Solutions.java

--- sai/Solutions.java
+++ sai/Solutions.java
@@ -254,3 +254,52 @@ class BoundedBlockingQueue {
         return boundedQueue.size();
     }
 }
+
+//Solution 2: Reentrant Lock
+  class BoundedBlockingQueue {
+    Queue<Integer> boundedQueue;
+    int capacity;
+    ReentrantLock lock;
+    Condition isFull;
+    Condition isEmpty;
+
+    public BoundedBlockingQueue(int capacity) {
+        this.capacity = capacity;
+        this.boundedQueue = new ArrayDeque<>(capacity);
+        this.lock = new ReentrantLock();
+        this.isEmpty = lock.newCondition();
+        this.isFull = lock.newCondition();
+    }
+
+    public void enqueue(int element) throws InterruptedException {
+        try {
+            lock.lock();
+            while (boundedQueue.size() == capacity) {
+                isFull.await();
+            }
+
+            boundedQueue.offer(element);
+            isEmpty.signalAll();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public int dequeue() throws InterruptedException {
+        try {
+            lock.lock();
+            while (boundedQueue.isEmpty()) {
+                isEmpty.await();
+            }
+            int value = boundedQueue.remove();
+            isFull.signalAll();
+            return value;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public int size() {
+        return boundedQueue.size();
+    }
+}
