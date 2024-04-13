877bde1b8dbd40ad5b4da623d0347b6bde36e424
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-04-13 11:53:40 +0530

    Create Solutions.java

---
+++ sai/Solutions.java
@@ -0,0 +1,179 @@
+/*1114. Print in Order
+
+Suppose we have a class:
+
+public class Foo {
+  public void first() { print("first"); }
+  public void second() { print("second"); }
+  public void third() { print("third"); }
+}
+
+Example 1:
+Input: nums = [1,2,3]
+Output: "firstsecondthird"
+Explanation: There are three threads being fired asynchronously. The input [1,2,3] means thread A calls first(), thread B calls second(), and thread C calls third(). "firstsecondthird" is the correct output.
+
+Example 2:
+Input: nums = [1,3,2]
+Output: "firstsecondthird"
+Explanation: The input [1,3,2] means thread A calls first(), thread B calls third(), and thread C calls second(). "firstsecondthird" is the correct output.
+*/
+//1 Synchronized Method : Here, the synchronized keyword is used on the methods themselves to lock the entire object. When a thread enters a synchronized method, it acquires the lock on the object and no other threads can enter any other synchronized method on the same object until the lock is released.
+class Foo {
+    private boolean oneDone;
+    private boolean twoDone;
+    
+    public Foo() {
+        oneDone = false;
+        twoDone = false;
+    }
+
+    public synchronized void first(Runnable printFirst) throws InterruptedException {
+        printFirst.run();
+        oneDone = true;
+        notifyAll();
+    }
+
+    public synchronized void second(Runnable printSecond) throws InterruptedException {
+        while (!oneDone) {
+            wait();
+        }
+        printSecond.run();
+        twoDone = true;
+        notifyAll();
+    }
+
+    public synchronized void third(Runnable printThird) throws InterruptedException {
+        while (!twoDone) {
+            wait();
+        }
+        printThird.run();
+    }
+}
+//2. Synchronized on Object : In this method, a separate Object is used as a lock instead of the class itself. When a thread enters a synchronized block with the lock object, it acquires the lock and no other threads can enter any other synchronized blocks with the same lock object until the lock is released.
+// For sychronize on object, using this to sync is enough( though it's OK to sync on another object)
+class Foo {
+    private Object lock;
+    private boolean oneDone;
+    private boolean twoDone;
+    
+    public Foo() {
+        lock = new Object();
+        oneDone = false;
+        twoDone = false;
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        synchronized (lock) {
+            printFirst.run();
+            oneDone = true;
+            lock.notifyAll();
+        }
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        synchronized (lock) {
+            while (!oneDone) {
+                lock.wait();
+            }
+            printSecond.run();
+            twoDone = true;
+            lock.notifyAll();
+        }
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        synchronized (lock) {
+            while (!twoDone) {
+                lock.wait();
+            }
+            printThird.run();
+        }
+    }
+}
+//3rd Solution - Semaphore
+class Foo {
+
+    Semaphore one = new Semaphore(1);
+    Semaphore two = new Semaphore(0);
+    Semaphore three = new Semaphore(0);
+
+    public Foo() {
+
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        one.acquire();
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        two.release();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        two.acquire();
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+        three.release();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        three.acquire();
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+        one.release();
+    }
+}
+
+////4th Condition Variable : A Condition Variable is a synchronization mechanism that provides a way to wait for a certain condition to be true before proceeding. Here, the await() method releases the lock and blocks the thread until another thread calls the signal() method. The Condition class is used in conjunction with a Lock.
+class Foo {
+    private Lock lock;
+    private boolean oneDone;
+    private boolean twoDone;
+    private Condition one;
+    private Condition two;
+    
+    public Foo() {
+        lock = new ReentrantLock();
+        one = lock.newCondition();
+        two = lock.newCondition();
+        oneDone = false;
+        twoDone = false;
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        lock.lock();
+        try {
+            printFirst.run();
+            oneDone = true;
+            one.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        lock.lock();
+        try {
+            while (!oneDone) {
+                one.await();
+            }
+            printSecond.run();
+            twoDone = true;
+            two.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        lock.lock();
+        try {
+            while (!twoDone) {
+                two.await();
+            }
+            printThird.run();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
