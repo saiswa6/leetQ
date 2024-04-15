5a856583c08dd2fd62ef79759aadd560df3a3f98
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-04-15 07:21:29 +0530

    Update Solutions.java

--- sai/Solutions.java
+++ sai/Solutions.java
@@ -2596,4 +2596,89 @@ public class ConcurrentList<T> {
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// Question : 
+// Question : 1226. The Dining Philosophers
+// Solution : 
+// Philospher
+package org.concurrency.Questions.DiningPhilosphers.Imp1Baeldung;
+
+public class Philosopher implements Runnable {
+
+    private Object leftFork ;
+    private Object rightFork;
+
+    public Philosopher(Object leftFork, Object rightFork){
+        this.leftFork = leftFork;
+        this.rightFork = rightFork;
+    }
+
+    @Override
+    public void run() {
+        try{
+            while (true) {
+                doAction(System.nanoTime() + "thinking");
+                synchronized (leftFork) {
+                    doAction(System.nanoTime() + " Picked up left Fork");
+                    synchronized (rightFork) {
+                        doAction(System.nanoTime() + "Picked up right Fork - eating");
+                        doAction(System.nanoTime() + "Picked down right Fork ");
+                    }
+                    doAction(System.nanoTime() + " Picked down left Fork. Back to thinking.");
+                }
+            }
+
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
+        }
+
+    }
+
+    private void doAction(String action) throws InterruptedException {
+        System.out.println(Thread.currentThread().getName() + " " + action);
+        Thread.sleep((int)(Math.random()*100));
+
+    }
+}
+//DiningPhilosphers
+package org.concurrency.Questions.DiningPhilosphers.Imp1Baeldung;
+
+import java.util.ArrayList;
+
+public class DiningPhilosphers {
+    public static void main(String[] args) {
+        Philosopher [] philosophers = new Philosopher[5];
+
+        Object[] forks = new Object[philosophers.length];
+        ArrayList<Thread> threads = new ArrayList<>();
+        for(int i = 0; i < philosophers.length; i++) {
+            forks[i] = new Object();
+        }
+
+        for(int i = 0; i < philosophers.length ; i++) {
+            Object leftFork = forks[i];
+            Object rightFork = forks[(i+1) % forks.length];
+
+            //philosophers[i] = new Philosopher(leftFork, rightFork);
+            // The last philosopher picks up the right fork first
+            if(i == philosophers.length - 1) {
+                philosophers[i] = new Philosopher(rightFork, leftFork);
+            } else {
+                philosophers[i] = new Philosopher(leftFork, rightFork);
+            }
+
+            Thread t = new Thread(philosophers[i], "Philosopher " + (i + 1));
+            threads.add(t);
+            t.start();
+        }
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        for(Thread thread : threads) {
+            thread.interrupt();
+        }
+    }
+}
+
