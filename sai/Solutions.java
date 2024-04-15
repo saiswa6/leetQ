7870c4bf6debe03f7fbd6c79d2a16f2512b50e29
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-04-15 11:27:41 +0530

    Update Solutions.java

--- sai/Solutions.java
+++ sai/Solutions.java
@@ -3290,4 +3290,157 @@ public class Database5 {
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//Question : 
+/*Question : Imagine at the end of a political conference, republicans and democrats are trying to leave the venue and ordering Uber rides at the same time.
+However, to make sure no fight breaks out in an Uber ride, the software developers at Uber come up with an algorithm whereby either an Uber ride can have all democrats or republicans or two Democrats and two Republicans. All other combinations can result in a fist-fight.
+Your task as the Uber developer is to model the ride requestors as threads. Once an acceptable combination of riders is possible, threads are allowed to proceed to ride. Each thread invokes the method seated() when selected by the system for the next ride. When all the threads are
+seated, any one of the four threads can invoke the method drive() to inform the driver to start the ride.*/
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class UberSeatingProblem {
+
+    private int republicans = 0;
+    private int democrats = 0;
+
+    private Semaphore demsWaiting  = new Semaphore(0);
+    private Semaphore repubsWaiting = new Semaphore(0);
+
+    CyclicBarrier cyclicBarrier = new CyclicBarrier(4);
+    ReentrantLock lock = new ReentrantLock();
+
+    public void drive() {
+        System.out.println("Uber ride is on way... with ride leader " + Thread.currentThread().getName());
+    }
+
+    public void seatDemocrat() throws InterruptedException, BrokenBarrierException {
+        boolean rideLeader = false;
+        lock.lock();
+
+        democrats++;
+
+        if(democrats == 4) {
+            //Seat all democrats in Ubder ride
+            demsWaiting.release(3);
+            democrats = democrats - 4;
+            rideLeader = true;
+        } else if(democrats == 2 && republicans >= 2) {
+            // Seat 2 democrats and 2 republicans
+            demsWaiting.release(1);
+            repubsWaiting.release(2);
+            democrats = democrats - 2;
+            republicans = republicans - 2;
+            rideLeader = true;
+
+        } else {
+            lock.unlock();
+            demsWaiting.acquire();
+        }
+
+        seated();
+        cyclicBarrier.await();
+
+        if(rideLeader) {
+            drive();
+            lock.unlock();
+        }
+    }
+
+    void seated() { // will call once they seat in car
+        System.out.println( Thread.currentThread().getName() + " seated");
+    }
+
+    public void seatRepulicam() throws InterruptedException, BrokenBarrierException {
+        boolean rideLeader = false;
+        lock.lock();
+
+        republicans++;
+
+        if(republicans == 4) {
+            //Seat all republicans in Ubder ride
+            repubsWaiting.release(3);
+            republicans = republicans - 4;
+            rideLeader = true;
+        } else if(republicans == 2 && democrats >= 2) {
+            // Seat 2 democrats and 2 republicans
+            repubsWaiting.release(1);
+            demsWaiting.release(2);
+            democrats = democrats - 2;
+            republicans = republicans - 2;
+            rideLeader = true;
+
+        } else {
+            lock.unlock();
+            repubsWaiting.acquire();
+        }
+
+        seated();
+        cyclicBarrier.await();
+
+        if(rideLeader) {
+            drive();
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        UberSeatingProblem uberSeatingProblem = new UberSeatingProblem();
+        Set<Thread> allThreads = new HashSet<>();
+
+
+        for(int i=0; i < 10;i++) {
+            Thread t =  new Thread(() -> {
+                try {
+                    uberSeatingProblem.seatDemocrat();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    System.out.println("problem occured");
+                    throw new RuntimeException(e);
+                }
+            });
+            t.setName("Democrat " + (i+1));
+            allThreads.add(t);
+
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        for(int i=0; i < 14;i++) {
+            Thread t =  new Thread(() -> {
+                try {
+                    uberSeatingProblem.seatRepulicam();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    System.out.println("problem occured");
+                    throw new RuntimeException(e);
+                }
+            });
+            t.setName("Republican " + (i+1));
+            allThreads.add(t);
+
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        for(Thread thread : allThreads) {
+            thread.start();
+        }
+
+
+        for(Thread thread : allThreads) {
+            thread.join();
+        }
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Question 
