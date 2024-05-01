4b0d213c5726c376d5716ff3ca3bd28076abdbd0
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-05-01 19:00:59 +0530

    Time: 16 ms (86.63%), Space: 53.3 MB (22.52%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -4,24 +4,23 @@ class Solution {
         int answer = 0;
 
         int length = grid.length;
-        for(int row = 0 ; row < length ; row++){
+        for (int row = 0; row < length; row++) { // First store all the rows in HashMap and increment frequency.
             List<Integer> list = new ArrayList<>(length);
-            for(int col = 0; col < length ; col++) {
+            for (int col = 0; col < length; col++) {
                 list.add(grid[row][col]);
             }
             rowMap.put(list, rowMap.getOrDefault(list, 0) + 1);
         }
 
-        for(int row = 0 ; row < length ; row++){
+        for (int row = 0; row < length; row++) { // Form the columns and check in the rowMap if exists.
             List<Integer> list = new ArrayList<>(length);
-            for(int col = 0; col < length ; col++) {
+            for (int col = 0; col < length; col++) {
                 list.add(grid[col][row]);
             }
             int count = rowMap.getOrDefault(list, 0);
-            if(count > 0) {
-                //rowMap.put(list, count - 1);
-                answer = answer + count;
-            } 
+            if (count > 0) {
+                answer = answer + count; // Add the number of times of occurence.
+            }
         }
         return answer;
     }
