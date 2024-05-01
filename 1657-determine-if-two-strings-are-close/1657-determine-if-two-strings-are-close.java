69153a70353713570dfdf1482b377f57fe797d73
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-05-01 18:25:30 +0530

    Time: 69 ms (29.49%), Space: 45.4 MB (59.98%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -1,21 +1,21 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
-        if(word1.length() != word2.length()) {
+        if (word1.length() != word2.length()) {
             return false;
         }
 
         Map<Character, Integer> word1Map = new HashMap<>();
         Map<Character, Integer> word2Map = new HashMap<>();
 
-        for(char c : word1.toCharArray()){
+        for (char c : word1.toCharArray()) {
             word1Map.put(c, word1Map.getOrDefault(c, 0) + 1);
         }
 
-        for(char c : word2.toCharArray()){
+        for (char c : word2.toCharArray()) {
             word2Map.put(c, word2Map.getOrDefault(c, 0) + 1);
         }
 
-        if(!word1Map.keySet().equals(word2Map.keySet())) {
+        if (!word1Map.keySet().equals(word2Map.keySet())) {
             return false;
         }
 
