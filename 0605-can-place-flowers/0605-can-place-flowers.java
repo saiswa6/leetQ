eccc9d0885271307b25b1d7664713ddb8086f218
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-05-01 13:49:45 +0530

    Time: 1 ms (95.54%), Space: 45.4 MB (61.42%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -1,24 +1,20 @@
-class Solution {
+public class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        if (n == 0) {
-            return true; // Important, directly retun true if n is 0.
-        }
-
-        int length = flowerbed.length;
-        for (int i = 0; i < length; i++) {
-            if (((i == 0) || (i - 1 < length && flowerbed[i - 1] == 0)) && flowerbed[i] == 0
-                    && ((i + 1 == length) || (i + 1 < length && flowerbed[i + 1] == 0))) {
-                // if n == 0 or i - 1 < length, check if it is 0.
-                // if currentIndex is 0.
-                // If nextIndex i + 1 < length or last Index (i + 1 == length).
-                n--;
-                if (n == 0) { // Important. If done, return true immediately.
-                    return true;
+        int count = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            // Check if the current plot is empty.
+            if (flowerbed[i] == 0) {
+                // Check if the left and right plots are empty.
+                boolean emptyLeftPlot = (i == 0) || (flowerbed[i - 1] == 0);
+                boolean emptyRightPlot = (i == flowerbed.length - 1) || (flowerbed[i + 1] == 0);
+                
+                // If both plots are empty, we can plant a flower here.
+                if (emptyLeftPlot && emptyRightPlot) {
+                    flowerbed[i] = 1;
+                    count++;
                 }
-                flowerbed[i] = 1; // Update the value for index where plant is planted for next iteration.
             }
         }
-
-        return n == 0;
+        return count >= n;
     }
 }
