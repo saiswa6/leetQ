f61445abf3ee97fae2cdf6bcbdf44942782a5726
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-05-01 11:13:51 +0530

    Time: 2 ms (79.91%), Space: 56.5 MB (6.45%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,26 +1,26 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int length = nums.length;
-        int prefix [] = new int[length];
-        int suffix [] = new int[length];
-        int result [] = new int[length];
+        int prefix[] = new int[length];
+        int suffix[] = new int[length];
+        int result[] = new int[length];
 
         int prefixProduct = 1;
         prefix[0] = prefixProduct;
-        for(int i = 1; i < length; i++) {
-            prefix[i] = prefixProduct * nums[i - 1];
-            prefixProduct = prefix[i];
+        for (int i = 1; i < length; i++) {
+            prefix[i] = prefixProduct * nums[i - 1]; // Multiply current prefixProduct with previous nums element
+            prefixProduct = prefix[i]; // Update prefixProduct for next iteration.
         }
-        
+
         int suffixProduct = 1;
         suffix[length - 1] = suffixProduct;
-        for(int i = length - 2; i >= 0; i--) {
-            suffix[i] = suffixProduct * nums[i + 1];
-            suffixProduct = suffix[i];
-        } 
+        for (int i = length - 2; i >= 0; i--) {
+            suffix[i] = suffixProduct * nums[i + 1]; // Multiply current suffixProduct with next nums element
+            suffixProduct = suffix[i]; // Update suffixProduct for next iteration
+        }
 
-        for(int k = 0; k < length ; k++) {
-            result[k] = prefix[k] * suffix[k];
+        for (int k = 0; k < length; k++) {
+            result[k] = prefix[k] * suffix[k]; // Finally merge
         }
 
         return result;
