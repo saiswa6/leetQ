fb53b363b9abbe9ce981d8b086a978a82fa6054f
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-05-01 15:21:28 +0530

    Update 0392-is-subsequence.java

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-     if(s.length() == 0) {
+     if(s.length() == 0) {  // Important
         return true;
      }
-     if(t.length() == 0) {
+     if(t.length() == 0) {  // Important
         return false;
      }
      int sPointer = 0;
      int tPointer = 0;
      for(tPointer = 0; tPointer < t.length() ; tPointer++) {
-        if(t.charAt(tPointer) == s.charAt(sPointer)) {
+        if(t.charAt(tPointer) == s.charAt(sPointer)) {  // Equal - increase pointer of s.
             sPointer++;
             if(sPointer == s.length()) {
                 return true;
