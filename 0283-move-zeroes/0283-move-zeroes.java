7f4d8a970496fd1e02d84db310de758b0af10378
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-05-01 14:59:42 +0530

    Time: 2 ms (86.16%), Space: 46.4 MB (10.22%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -2,18 +2,18 @@ class Solution {
     public void moveZeroes(int[] nums) {
         int length = nums.length;
         int pointer = 0;
-        int k = 0;
+        int k = 0; // to track zeroes
 
-        for( ; pointer < length ; pointer++) {
-            if(nums[pointer] != 0) {
-                int temp = nums[pointer];
-                nums[pointer] = nums[k];
-                nums[k] = temp;
+        for (; pointer < length; pointer++) {
+            if (nums[pointer] != 0) {
+                // int temp = nums[pointer]; // No need to do swap
+                // nums[pointer] = nums[k];
+                nums[k] = nums[pointer];
                 k++;
             }
         }
 
-        for( ; k < length ; k++) {
+        for (; k < length; k++) {
             nums[k] = 0;
         }
     }
