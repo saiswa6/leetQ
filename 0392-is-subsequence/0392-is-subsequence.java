27e5dcc47387bed0064c69435288eb1fc44ec23a
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-05-01 15:32:38 +0530

    Time: 1 ms (90.62%), Space: 41.5 MB (33.57%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-     if(s.length() == 0) {  // Important
+     if(s.length() == 0) {
         return true;
      }
-     if(t.length() == 0) {  // Important
+     if(t.length() == 0) {
         return false;
      }
      int sPointer = 0;
      int tPointer = 0;
      for(tPointer = 0; tPointer < t.length() ; tPointer++) {
-        if(t.charAt(tPointer) == s.charAt(sPointer)) {  // Equal - increase pointer of s.
+        if(t.charAt(tPointer) == s.charAt(sPointer)) {
             sPointer++;
             if(sPointer == s.length()) {
                 return true;
