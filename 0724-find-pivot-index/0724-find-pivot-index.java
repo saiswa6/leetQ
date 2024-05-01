dba1603d8ded3d70d6cd3c70660b8e26dbfecf99
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-05-01 17:09:28 +0530

    Time: 1 ms (96.34%), Space: 45.6 MB (26.47%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,24 +1,26 @@
 class Solution {
     public int pivotIndex(int[] nums) {
         int length = nums.length;
-        int prefixSum [] = new int[length];
-        int suffixSum[] = new int [length];
+        int prefixSum[] = new int[length];
+        int suffixSum[] = new int[length];
 
         prefixSum[0] = nums[0];
-        for(int i = 1; i < length; i++) {
-            prefixSum[i] = prefixSum[i - 1] + nums[i];
+        for (int i = 1; i < length; i++) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i]; // Be careful while calculating prefixSum.
+            // Last prefixSum Element + current Element
         }
 
         suffixSum[length - 1] = nums[length - 1];
-        for(int i = length - 2; i >= 0; i--) {
-            suffixSum[i] = suffixSum[i + 1] + nums[i];
+        for (int i = length - 2; i >= 0; i--) {
+            suffixSum[i] = suffixSum[i + 1] + nums[i]; // Be careful while calculating prefixSum.
+            // Next suffixSum Element + current Element
         }
 
-        for(int i = 0; i < length; i++) {
-            if(prefixSum[i] == suffixSum[i]) {
+        for (int i = 0; i < length; i++) {
+            if (prefixSum[i] == suffixSum[i]) {// FInd first one from left side.
                 return i;
             }
         }
-        return -1; 
+        return -1;
     }
 }
