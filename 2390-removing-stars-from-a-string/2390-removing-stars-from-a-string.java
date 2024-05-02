4f7be1a5b4de888dd7365a8ab45c8d8c0f4e4a2c
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-05-02 09:46:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String removeStars(String s) {
+        char sArray[] = s.toCharArray();
+        int length = s.length();
+
+        for (int i = 0; i < length; i++) { // '0' is used to mark char as non existent as deletion is costly.
+            if (sArray[i] == '*') { // If '*' is found, traverse back to see first non star and also check not '0'
+                for (int j = i - 1; j >= 0; j--) {
+                    if (sArray[j] != '0') { // Replace with '0' when non-star and '0'
+                        sArray[j] = '0';
+                        break; // Important break after one char is found.
+                    }
+                }
+                sArray[i] = '0'; // Mark '*' also '0' to indicate as deleted
+            }
+        }
+
+        StringBuilder stringBuilder = new StringBuilder(); // Traverse and append
+        for (int i = 0; i < length; i++) {
+            if (sArray[i] != '0') {
+                stringBuilder.append(sArray[i]);
+            }
+        }
+
+        return stringBuilder.toString();
+    }
+}
