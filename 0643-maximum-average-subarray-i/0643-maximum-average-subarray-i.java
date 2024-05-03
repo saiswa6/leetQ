234c95ebf12207b0c8ef2677cd270bed6f2b5459
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-05-03 10:35:11 +0530

    Time: 6 ms (16.67%), Space: 55.5 MB (73.09%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -2,8 +2,11 @@ class Solution {
     public double findMaxAverage(int[] nums, int k) {
         int sum = 0;
         int start = 0;
-        double answer = -Double.MAX_VALUE;
-
+        double answer = -Double.MAX_VALUE; // (Link :
+                                           // https://stackoverflow.com/questions/3884793/why-is-double-min-value-in-not-negative#:~:text=in%20the%20documents%2C-,Double.,%2C%202%5E(%2D1074).&text=The%20mantissa%2C%20always%20a%20positive,of%20the%20floating%2Dpoint%20number.)
+        // Double.MIN_VALUE represent Double.MIN_VALUE is the least positive value
+        // (since that represents the least possible magnitude)
+        // Double.MAX_VALUE should be seen as maximum magnitude
         for (int end = 0; end < nums.length; end++) {
             sum += nums[end];
 
