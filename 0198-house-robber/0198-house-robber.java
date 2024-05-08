e60e576bfd9e80d013120abb06c96a55a1e86fb9
saiswa6 <111691459+saiswa6@users.noreply.github.com>
2024-05-08 16:38:54 +0530

    Time: 0 ms (100%), Space: 41.2 MB (12.59%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,22 +1,49 @@
 class Solution {
-;
+    ;
     public int rob(int[] nums) {
         int n = nums.length;
         int dp[] = new int[n];
-        return robHelper(n - 1 , nums);
+        Arrays.fill(dp, -1);
+        return robHelper(n - 1, nums, dp);
     }
 
-    public int robHelper(int index, int nums[]) {
-        if(index == 0) {
+    public int robHelper(int index, int nums[], int dp[]) {
+        if (index == 0) {
             return nums[index];
         }
-        if(index < 0) {
+        if (index < 0) {
             return 0;
         }
+        if (dp[index] != -1) {
+            return dp[index];
+        }
 
-        int pick = nums[index] + robHelper(index - 2, nums);
-        int notPick = 0 + robHelper(index - 1, nums);
+        int pick = nums[index] + robHelper(index - 2, nums, dp);
+        int notPick = 0 + robHelper(index - 1, nums, dp);
 
-        return Math.max(pick, notPick);
+        dp[index] = Math.max(pick, notPick);
+        return dp[index];
     }
+
+    /*
+     * public int rob(int[] nums) {
+     * int n = nums.length;
+     * int dp[] = new int[n];
+     * return robHelper(n - 1 , nums);
+     * }
+     * 
+     * public int robHelper(int index, int nums[]) {
+     * if(index == 0) {
+     * return nums[index];
+     * }
+     * if(index < 0) {
+     * return 0;
+     * }
+     * 
+     * int pick = nums[index] + robHelper(index - 2, nums);
+     * int notPick = 0 + robHelper(index - 1, nums);
+     * 
+     * return Math.max(pick, notPick);
+     * }
+     */
 }
